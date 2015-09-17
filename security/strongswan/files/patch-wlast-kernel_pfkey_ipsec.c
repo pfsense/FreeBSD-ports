10af894aaac355f87595cfeb4a452230743d2688
Renato Botelho <garga@FreeBSD.org>
2015-09-17 16:07:20 -0300

    Update security/strongswan to 5.3.3

--- security/strongswan/files/patch-wlast-kernel_pfkey_ipsec.c
+++ security/strongswan/files/patch-wlast-kernel_pfkey_ipsec.c
@@ -1,6 +1,6 @@
---- src/libhydra/plugins/kernel_pfkey/kernel_pfkey_ipsec.c.orig	2014-11-30 21:32:24.000000000 +0100
-+++ src/libhydra/plugins/kernel_pfkey/kernel_pfkey_ipsec.c	2014-11-30 21:33:03.000000000 +0100
-@@ -123,7 +123,7 @@
+--- ./src/libhydra/plugins/kernel_pfkey/kernel_pfkey_ipsec.c.orig	2015-09-17 13:59:01.303975920 -0500
++++ ./src/libhydra/plugins/kernel_pfkey/kernel_pfkey_ipsec.c	2015-09-17 14:01:17.305961648 -0500
+@@ -129,7 +129,7 @@
  
  /* from linux/udp.h */
  #ifndef UDP_ENCAP
@@ -9,7 +9,7 @@
  #endif
  
  #ifndef UDP_ENCAP_ESPINUDP
-@@ -822,13 +822,13 @@
+@@ -843,14 +843,14 @@
  /*	{ENCR_DES_IV32,				0							}, */
  	{ENCR_NULL,					SADB_EALG_NULL				},
  	{ENCR_AES_CBC,				SADB_X_EALG_AESCBC			},
@@ -18,12 +18,13 @@
  /*  {ENCR_AES_CCM_ICV8,			SADB_X_EALG_AES_CCM_ICV8	}, */
  /*	{ENCR_AES_CCM_ICV12,		SADB_X_EALG_AES_CCM_ICV12	}, */
  /*	{ENCR_AES_CCM_ICV16,		SADB_X_EALG_AES_CCM_ICV16	}, */
--/*	{ENCR_AES_GCM_ICV8,			SADB_X_EALG_AES_GCM_ICV8	}, */
--/*	{ENCR_AES_GCM_ICV12,		SADB_X_EALG_AES_GCM_ICV12	}, */
--/*	{ENCR_AES_GCM_ICV16,		SADB_X_EALG_AES_GCM_ICV16	}, */
+ #ifdef SADB_X_EALG_AES_GCM_ICV8 /* assume the others are defined too */
+-	{ENCR_AES_GCM_ICV8,			SADB_X_EALG_AES_GCM_ICV8	},
+-	{ENCR_AES_GCM_ICV12,		SADB_X_EALG_AES_GCM_ICV12	},
+-	{ENCR_AES_GCM_ICV16,		SADB_X_EALG_AES_GCM_ICV16	},
 +	{ENCR_AES_GCM_ICV8,		SADB_X_EALG_AESGCM8	},
 +	{ENCR_AES_GCM_ICV12,		SADB_X_EALG_AESGCM12	},
 +	{ENCR_AES_GCM_ICV16,		SADB_X_EALG_AESGCM16	},
+ #endif
  	{END_OF_LIST,				0							},
  };
- 
