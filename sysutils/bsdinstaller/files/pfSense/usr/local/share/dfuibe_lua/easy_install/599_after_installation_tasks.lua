47c5a27d66b18e78653ddcea13d30d268cd5be76
Renato Botelho <garga@FreeBSD.org>
2015-09-30 13:51:47 -0300

    Call after_installation_routines.sh direct instead of executing line by line

--- sysutils/bsdinstaller/files/pfSense/usr/local/share/dfuibe_lua/easy_install/599_after_installation_tasks.lua
+++ sysutils/bsdinstaller/files/pfSense/usr/local/share/dfuibe_lua/easy_install/599_after_installation_tasks.lua
@@ -2,9 +2,6 @@
 --
 --  pfSense after installation routines
 --
---  Loop through io.lines(filename) and
---  run each command listed in file.
---
 --  This file cleans up after a normal install.
 --
 
@@ -13,21 +10,10 @@ return {
     name = _("pfSense after installation routines"),
     effect = function(step)
 	local cmds = CmdChain.new()
-	local filename = "/usr/local/bin/after_installation_routines.sh"
-	local line
 
 	-- Label partitions
 	cmds:add("/usr/bin/env DESTDIR=/mnt /bin/sh /usr/local/sbin/ufslabels.sh commit")
-
-	for line in io.lines(filename) do
-		cmds:set_replacements{
-		    line = line,
-		    base = App.state.target:get_base()
-		}
-		if not string.find(line, "^%#") then
-		    cmds:add("${line}")
-		end
-	end
+	cmds:add("/bin/sh /usr/local/bin/after_installation_routines.sh")
 
 	cmds:execute()
 	
