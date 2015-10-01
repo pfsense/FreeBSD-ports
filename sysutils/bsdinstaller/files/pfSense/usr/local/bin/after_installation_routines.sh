1cdc956289f72b202e3fc2f8c07c3a101f530697
Renato Botelho <garga@FreeBSD.org>
2015-10-01 07:07:49 -0300

    Improve readability of after_installation_routines.sh since it's now direct executed instead of read line by line, no functional changes

--- sysutils/bsdinstaller/files/pfSense/usr/local/bin/after_installation_routines.sh
+++ sysutils/bsdinstaller/files/pfSense/usr/local/bin/after_installation_routines.sh
@@ -29,20 +29,34 @@ cp /tmp/installer.log /mnt/var/installer_logs
 cp /tmp/install-session.sh /mnt/var/installer_logs
 cp /tmp/new.fdisk /mnt/var/installer_logs
 
+export ASSUME_ALWAYS_YES=true
+
 # Remove bsdinstaller from target
-/usr/bin/env ASSUME_ALWAYS_YES=true /usr/sbin/pkg -c /mnt delete -f -q -y bsdinstaller
+/usr/sbin/pkg -c /mnt delete -f -q -y bsdinstaller
 
 # Fix default-config package
-/usr/bin/env ASSUME_ALWAYS_YES=true /usr/sbin/pkg -c /mnt delete -f -q -y ${product}-default-config\*
+/usr/sbin/pkg -c /mnt delete -f -q -y ${product}-default-config\*
+
 cp -r /pkgs /mnt
-if [ -f /mnt/boot.config ]; then /usr/bin/env ASSUME_ALWAYS_YES=true /usr/sbin/pkg -c /mnt add -fq /pkgs/${product}-default-config-serial-[0-9]*.txz; else /usr/bin/env ASSUME_ALWAYS_YES=true /usr/sbin/pkg -c /mnt add -fq /pkgs/pfSense-default-config-[0-9]*.txz; fi;
+if [ -f /mnt/boot.config ]; then
+	/usr/sbin/pkg -c /mnt add -fq /pkgs/${product}-default-config-serial-[0-9]*.txz
+else
+	/usr/sbin/pkg -c /mnt add -fq /pkgs/pfSense-default-config-[0-9]*.txz
+fi
 rm -rf /mnt/pkgs
 
 # If the platform is vmware, lets do some fixups.
-if [ -f /var/IS_VMWARE ]; then echo "" >> /mnt/etc/sysctl.conf; echo "kern.timecounter.hardware=i8254" >> /mnt/etc/sysctl.conf; echo kern.hz="100" >> /mnt/boot/loader.conf; fi;
+if [ -f /var/IS_VMWARE ]; then
+	echo "" >> /mnt/etc/sysctl.conf
+	echo "kern.timecounter.hardware=i8254" >> /mnt/etc/sysctl.conf
+	echo kern.hz="100" >> /mnt/boot/loader.conf
+fi
 
 # Fixup permissions on installed files
-if [ -f /usr/local/share/${product}/base.mtree ]; then /usr/sbin/mtree -U -e -q -f /usr/local/share/${product}/base.mtree -p /mnt/ > /mnt/cf/conf/mtree.log; fi;
+if [ -f /usr/local/share/${product}/base.mtree ]; then
+	/usr/sbin/mtree -U -e -q -f /usr/local/share/${product}/base.mtree \
+		-p /mnt/ > /mnt/cf/conf/mtree.log
+fi
 
 #Sync disks
 /bin/sync
