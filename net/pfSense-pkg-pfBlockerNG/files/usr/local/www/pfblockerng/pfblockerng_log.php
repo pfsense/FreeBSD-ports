68a04c642c1b7a5e42489cee38fae23ea1df23c3
BBcan177 <bbcan177@gmail.com>
2015-12-11 11:21:01 -0500

    Update pfblockerng_log.php

--- net/pfSense-pkg-pfBlockerNG/files/usr/local/www/pfblockerng/pfblockerng_log.php
+++ net/pfSense-pkg-pfBlockerNG/files/usr/local/www/pfblockerng/pfblockerng_log.php
@@ -343,7 +343,7 @@ $section->addInput(new Form_StaticText(
 	'<div style="display:none;" id="fileStatusBox"><strong id="fileStatus"></strong></div>'
 	. '<div style="display:none;" id="filePathBox"><strong>Log/File Path:&emsp;</strong>'
 	. '<div style="display:inline;" id="fbTarget"></div>'
-	. '<div class="pull-right" display:inline;" id="fileRefreshBtn">'
+	. '<div style="display:inline; margin-right:10px;" class="pull-right" id="fileRefreshBtn">'
 	. $logbtns . '</div>'
 ));
 $form->add($section);
