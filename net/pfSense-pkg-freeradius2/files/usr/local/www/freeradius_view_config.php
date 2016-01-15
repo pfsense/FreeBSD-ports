09c2433a66e1a97370c477e5cd4a601200bd1b8a
jim-p <jimp@pfsense.org>
2016-01-15 14:22:32 -0500

    Convert FreeRADIUS2 config viewer to Bootstrap (rest of the pkg is XML-based). Implements #5572

--- net/pfSense-pkg-freeradius2/files/usr/local/www/freeradius_view_config.php
+++ net/pfSense-pkg-freeradius2/files/usr/local/www/freeradius_view_config.php
@@ -31,18 +31,7 @@
 */
 
 require("guiconfig.inc");
-
-// Check to find out on which system the package is running
-$pfs_version = substr(trim(file_get_contents("/etc/version")),0,3);
-if ($pfs_version == "2.1") {
-	define('RADDB', '/usr/pbi/freeradius-' . php_uname("m") . '/etc/raddb');
-} else if ($pfs_version == "2.1") {
-	define('RADDB', '/usr/pbi/freeradius-' . php_uname("m") . '/local/etc/raddb');
-} else {
-	define('RADDB', '/usr/local/etc/raddb');
-}
-// End of system check
-
+define('RADDB', '/usr/local/etc/raddb');
 
 function get_file($file){
 	$files['radiusd']=RADDB . "/radiusd.conf";
@@ -60,113 +49,104 @@ function get_file($file){
 
 
 	if ($files[$file]!="" && file_exists($files[$file])){
-		print '<textarea rows="50" cols="100%">';
+		print '<pre>';
 		print $files[$file]."\n".file_get_contents($files[$file]);
-		print '</textarea>';
+		print '</pre>';
 	}
 }
 
-if ($_REQUEST['file']!=""){
+if ($_REQUEST['file'] != "") {
 	get_file($_REQUEST['file']);
-	}
-else{
-	$pgtitle = "FreeRADIUS: View Configuration";
-	include("head.inc");
-	include("fbegin.inc");
-	?>
+	return;
+}
 
-	<?php if ($savemsg) print_info_box($savemsg); ?>
-	
-	<form action="freeradius_view_config.php" method="post">
-		
-	<div id="mainlevel">
-		<table width="100%" border="0" cellpadding="0" cellspacing="0">
-			<tr><td>
-	<?php
-		$tab_array = array();
-		$tab_array[] = array(gettext("Users"), false, "/pkg.php?xml=freeradius.xml");
-		$tab_array[] = array(gettext("MACs"), false, "/pkg.php?xml=freeradiusauthorizedmacs.xml");
-		$tab_array[] = array(gettext("NAS / Clients"), false, "/pkg.php?xml=freeradiusclients.xml");
-		$tab_array[] = array(gettext("Interfaces"), false, "/pkg.php?xml=freeradiusinterfaces.xml");
-		$tab_array[] = array(gettext("Settings"), false, "/pkg_edit.php?xml=freeradiussettings.xml&id=0");
-		$tab_array[] = array(gettext("EAP"), false, "/pkg_edit.php?xml=freeradiuseapconf.xml&id=0");
-		$tab_array[] = array(gettext("SQL"), false, "/pkg_edit.php?xml=freeradiussqlconf.xml&id=0");
-		$tab_array[] = array(gettext("Certificates"), false, "/pkg_edit.php?xml=freeradiuscerts.xml&id=0");
-		$tab_array[] = array(gettext("LDAP"), false, "/pkg_edit.php?xml=freeradiusmodulesldap.xml&id=0");
-		$tab_array[] = array(gettext("View config"), true, "/freeradius_view_config.php");
-		$tab_array[] = array(gettext("XMLRPC Sync"), false, "/pkg_edit.php?xml=freeradiussync.xml&amp;id=0");
-		display_top_tabs($tab_array);
-	?>
-			</td></tr>
-			<tr>
-				<td>
-					<div id="mainarea">
-						<table class="tabcont" width="100%" border="0" cellpadding="8" cellspacing="0">
-						<tr><td></td></tr>
-						<tr>
-						<td colspan="2" valign="top" class="listtopic"><?=gettext("View freeRADIUS configuration files"); ?></td></tr>
-						<tr><td></td></tr>
-						</tr>
-							<tr>
-							<td class="tabcont" >
-							<input type="button" onClick="get_freeradius_file('radiusd');" id='btn_radiusd' value="radiusd.conf">&nbsp;
-							<input type="button" onClick="get_freeradius_file('eap');" id='btn_eap' value="eap.conf">&nbsp;
-							<input type="button" onClick="get_freeradius_file('sql');" id='btn_sql' value="sql.conf">&nbsp;
-							<input type="button" onClick="get_freeradius_file('clients');" id='btn_clients' value="clients.conf">&nbsp;
-							<input type="button" onClick="get_freeradius_file('users');" id='btn_users' value="users">&nbsp;
-							<input type="button" onClick="get_freeradius_file('macs');" id='btn_macs' value="macs">&nbsp;
-							<input type="button" onClick="get_freeradius_file('virtual-server-default');" id='btn_virtual-server-default' value="virtual-server-default">&nbsp;
-							<input type="button" onClick="get_freeradius_file('ca');" id='btn_ca' value="ca.cnf">&nbsp;
-							<input type="button" onClick="get_freeradius_file('server');" id='btn_server' value="server.cnf">&nbsp;
-							<input type="button" onClick="get_freeradius_file('client');" id='btn_client' value="client.cnf">&nbsp;
-							<input type="button" onClick="get_freeradius_file('index');" id='btn_index' value="index.txt">&nbsp;
-							<input type="button" onClick="get_freeradius_file('ldap');" id='btn_ldap' value="ldap">&nbsp;
-							</td>
-								</tr>
-								<tr>
-								<td class="tabcont" >
-								<div id="file_div"></div>
-									
-								</td>
-							</tr>
-						</table>
-					</div>
-				</td>
-			</tr>
-		</table>
+$pgtitle = array(gettext("Package"), gettext("FreeRADIUS"), gettext("View Configuration"));
+require("head.inc");
+
+$tab_array = array();
+$tab_array[] = array(gettext("Users"), false, "/pkg.php?xml=freeradius.xml");
+$tab_array[] = array(gettext("MACs"), false, "/pkg.php?xml=freeradiusauthorizedmacs.xml");
+$tab_array[] = array(gettext("NAS / Clients"), false, "/pkg.php?xml=freeradiusclients.xml");
+$tab_array[] = array(gettext("Interfaces"), false, "/pkg.php?xml=freeradiusinterfaces.xml");
+$tab_array[] = array(gettext("Settings"), false, "/pkg_edit.php?xml=freeradiussettings.xml&id=0");
+$tab_array[] = array(gettext("EAP"), false, "/pkg_edit.php?xml=freeradiuseapconf.xml&id=0");
+$tab_array[] = array(gettext("SQL"), false, "/pkg_edit.php?xml=freeradiussqlconf.xml&id=0");
+$tab_array[] = array(gettext("Certificates"), false, "/pkg_edit.php?xml=freeradiuscerts.xml&id=0");
+$tab_array[] = array(gettext("LDAP"), false, "/pkg_edit.php?xml=freeradiusmodulesldap.xml&id=0");
+$tab_array[] = array(gettext("View config"), true, "/freeradius_view_config.php");
+$tab_array[] = array(gettext("XMLRPC Sync"), false, "/pkg_edit.php?xml=freeradiussync.xml&amp;id=0");
+display_top_tabs($tab_array);
+
+if ($savemsg) {
+	print_info_box($savemsg);
+}
+?>
+
+<div class="panel panel-default">
+	<div class="panel-heading"><h2 class="panel-title"><?=gettext("View FreeRADIUS Configuration Files"); ?></h2></div>
+	<div class="panel-body">
+		<div class="table-responsive">
+			<form action="freeradius_view_config.php" method="post">
+			<table class="table table-hover table-condensed">
+				<thead>
+				<tr>
+					<th><input type="button" onClick="get_freeradius_file('radiusd');" id='btn_radiusd' value="radiusd.conf"></th>
+					<th><input type="button" onClick="get_freeradius_file('eap');" id='btn_eap' value="eap.conf"></th>
+					<th><input type="button" onClick="get_freeradius_file('sql');" id='btn_sql' value="sql.conf"></th>
+					<th><input type="button" onClick="get_freeradius_file('clients');" id='btn_clients' value="clients.conf"></th>
+					<th><input type="button" onClick="get_freeradius_file('users');" id='btn_users' value="users"></th>
+					<th><input type="button" onClick="get_freeradius_file('macs');" id='btn_macs' value="macs"></th>
+					<th><input type="button" onClick="get_freeradius_file('virtual-server-default');" id='btn_virtual-server-default' value="virtual-server-default"></th>
+					<th><input type="button" onClick="get_freeradius_file('ca');" id='btn_ca' value="ca.cnf"></th>
+					<th><input type="button" onClick="get_freeradius_file('server');" id='btn_server' value="server.cnf"></th>
+					<th><input type="button" onClick="get_freeradius_file('client');" id='btn_client' value="client.cnf"></th>
+					<th><input type="button" onClick="get_freeradius_file('index');" id='btn_index' value="index.txt"></th>
+					<th><input type="button" onClick="get_freeradius_file('ldap');" id='btn_ldap' value="ldap"></th>
+				</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td colspan="12">
+							<div id="file_div">Click one of the buttons above to display its contents.</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			</form>
+		</div>
 	</div>
-	</form>
-	<script type="text/javascript">
-	function get_freeradius_file(file) {
-			$('btn_'+file).value="reading...";
-			var pars = 'file='+file;
-			var url = "/freeradius_view_config.php";
-			var myAjax = new Ajax.Request(
-				url,
-				{
-					method: 'post',
-					parameters: pars,
-					onComplete: activitycallback_postfix_file
-				});
-			}
-		function activitycallback_postfix_file(transport) {
-			$('file_div').innerHTML = transport.responseText;
-			$('btn_radiusd').value="radiusd.conf";
-			$('btn_eap').value="eap.conf";
-			$('btn_sql').value="sql.conf";
-			$('btn_clients').value="clients.conf";
-			$('btn_users').value="users";
-			$('btn_macs').value="macs";
-			$('btn_virtual').value="virtual-server-default";
-			$('btn_ca').value="ca.cnf";
-			$('btn_server').value="server.cnf";
-			$('btn_client').value="client.cnf";
-			$('btn_index').value="index.txt";
-			$('btn_ldap').value="ldap";
-			scroll(0,0);
+</div>
+<script type="text/javascript">
+//<![CDATA[
+function get_freeradius_file(file) {
+	$('#btn_'+file).value="reading...";
+	var pars = 'file='+file;
+	var url = "/freeradius_view_config.php";
+	jQuery.ajax(url,
+		{
+		type: 'post',
+		data: pars,
+		success: activitycallback_freeradius_file
 		}
-	</script>
-	<?php 
-	include("foot.inc"); 
-	}
-	?>
+		);
+}
+function activitycallback_freeradius_file(html) {
+	$('#file_div').html(html);
+	$('#btn_radiusd').value="radiusd.conf";
+	$('#btn_eap').value="eap.conf";
+	$('#btn_sql').value="sql.conf";
+	$('#btn_clients').value="clients.conf";
+	$('#btn_users').value="users";
+	$('#btn_macs').value="macs";
+	$('#btn_virtual').value="virtual-server-default";
+	$('#btn_ca').value="ca.cnf";
+	$('#btn_server').value="server.cnf";
+	$('#btn_client').value="client.cnf";
+	$('#btn_index').value="index.txt";
+	$('#btn_ldap').value="ldap";
+	scroll(0,0);
+}
+//]]>
+</script>
+
+<?php include("foot.inc"); ?>
