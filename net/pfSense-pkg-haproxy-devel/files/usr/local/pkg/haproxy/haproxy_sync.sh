9a07fac3d8981e25d15ca2dd03ccfd2e592d2a77
PiBa-NL <pba_2k3@yahoo.com>
2016-01-18 02:28:11 +0100

    haproxy-devel, change haproxy_sync.sh line endings to LF

--- net/pfSense-pkg-haproxy-devel/files/usr/local/pkg/haproxy/haproxy_sync.sh
+++ net/pfSense-pkg-haproxy-devel/files/usr/local/pkg/haproxy/haproxy_sync.sh
@@ -5,4 +5,3 @@ require_once('haproxy.inc');
 echo "HAProxy Start sync ". (new DateTime('NOW'))->format('c') . "\n";
 haproxy_do_xmlrpc_sync();
 echo "HAProxy Stop sync ". (new DateTime('NOW'))->format('c') . "\n";
-
