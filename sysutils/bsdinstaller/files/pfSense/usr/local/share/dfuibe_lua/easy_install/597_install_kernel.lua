53c45526a0c11c03202a7caf014856bba97d719c
Jeremy Porter <jporter@pfsense.org>
2016-03-17 18:48:05 -0500

    factory serializer

--- sysutils/bsdinstaller/files/pfSense/usr/local/share/dfuibe_lua/easy_install/597_install_kernel.lua
+++ sysutils/bsdinstaller/files/pfSense/usr/local/share/dfuibe_lua/easy_install/597_install_kernel.lua
@@ -31,29 +31,83 @@ return {
 	local datasets_list = {}
 	local actions_list = {}
 	
-	local is_adi = (os.execute("/bin/kenv -q smbios.system.product | /usr/bin/egrep -q 'RCC-VE|DFFv2'") == 0)
+	local is_adi = (os.execute("/bin/kenv -q smbios.system.product | /usr/bin/egrep -q 'RCC-VE|DFFv2|RCC'") == 0)
 
 	if is_adi then
-		actions_list = {
-			{
-			    id = "Embedded",
-			    name = _("Embedded kernel (no VGA console, keyboard")
-			},
-			{
-			    id = "SMP",
-			    name = _("Standard Kernel")
-			}
+ 		actions = { 
+	                {
+	                    id = "SG-2220",
+	                    name = _("SG-2220")
+	                },
+	                {
+	                    id = "SG-2440",
+	                    name = _("SG-2440")
+	                },
+	                {
+	                    id = "SG-4860",
+	                    name = _("SG-4860")
+	                },
+	                {
+	                    id = "SG-4860-1U",
+	                    name = _("SG-4860-1U")
+	                },
+	                {
+	                    id = "SG-8860-1U",
+	        	    name = _("SG-8860-1U")
+			}, 
+	                {
+	                    id = "SG-2220",
+	                    name = _("SG-2220")
+	                },
+	                {
+	                    id = "SG-2440",
+	                    name = _("SG-2440")
+	                },
+	                {
+	                    id = "SG-4860",
+	                    name = _("SG-4860")
+	                },
+	                {
+	                    id = "SG-4860-1U",
+	                    name = _("SG-4860-1U")
+	                },
+	                {
+	                    id = "SG-8860-1U",
+	                    name = _("SG-8860-1U")
+	                },
+	                {
+	                    id = "SG-8860",
+	                    name = _("SG-8860")
+	                },
+	                {
+	                    id = "XG-2758",
+	                    name = _("XG-2758")
+	                },
+	                {
+	                    id = "Default",
+	                    name = _("Other/not listed")
+	                }
 		}
 	else
 		actions_list = {
 			{
-			    id = "SMP",
-			    name = _("Standard Kernel")
-			},
+	                    id = "C2758",
+	                    name = _("C2758")
+	                },
+	                {
+	                    id = "APU",
+	                    name = _("APU")
+	                },
+			 {
+	                    id = "XG-1540",
+	                    name = _("XG-1540")
+	                },
 			{
-			    id = "Embedded",
-			    name = _("Embedded kernel (no VGA console, keyboard")
-			}
+	                    id = "Default",
+	                    name = _("Other/not listed")
+	                }
+	
+
 		}
 	end
 
@@ -75,7 +129,7 @@ return {
 	    extensible = "false"
 	})
 
-	if response.action_id == "Embedded" then
+	if response.action_id != "Default" then
 		local cmds = CmdChain.new()
 		if is_adi then
 			cmds:add("echo -S115200 -h >> /mnt/boot.config")
@@ -99,6 +153,7 @@ return {
 
 		cmds:execute()
 	end
+	App.state.netgate_model = response.action_id
 
 	return step:next()
 
