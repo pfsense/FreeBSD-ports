51b6d57fc16a62fd0fe43ef19db23605c914b42a
Treer <treer.git@the-bordello.com>
2016-08-14 03:33:23 +1000

    Converted first page to Bootstrap
    (cherry picked from commit 30db6af8b91ff6d9d114f00480044fe74f8a524b)

--- sysutils/pfSense-pkg-LCDproc/files/usr/local/www/packages/lcdproc/lcdproc.php
+++ sysutils/pfSense-pkg-LCDproc/files/usr/local/www/packages/lcdproc/lcdproc.php
@@ -33,36 +33,63 @@
 require_once("guiconfig.inc");
 require_once("/usr/local/pkg/lcdproc.inc");
 
-$a_lcdproc = &$config['lcdproc']['item'];
+$lcdproc_config         = &$config['installedpackages']['lcdproc']['config'][0];
+$lcdproc_screens_config = &$config['installedpackages']['lcdprocscreens']['config'][0];
 
-/*
-if ($_GET['act'] == "del") {
-	if ($_GET['type'] == 'php') {
-		if ($a_lcdproc[$_GET['id']]) {
-			unset($a_lcdproc[$_GET['id']]);
-			write_config();
-			header("Location: lcdproc.php");
-			exit;
-		}
+// Set default values for anything not in the $config
+$pconfig = $lcdproc_config;
+if (!isset($pconfig['enabled']))           $pconfig['enabled']           = 'no';
+if (!isset($pconfig['comport']))           $pconfig['enabled']           = 'ucom1';
+if (!isset($pconfig['size']))              $pconfig['size']              = '16x2';
+if (!isset($pconfig['driver']))            $pconfig['driver']            = 'pyramid';
+if (!isset($pconfig['connection_type']))   $pconfig['connection_type']   = 'lcd2usb';
+if (!isset($pconfig['refresh_frequency'])) $pconfig['refresh_frequency'] = '5';
+if (!isset($pconfig['port_speed']))        $pconfig['port_speed']        = '0';
+if (!isset($pconfig['brightness']))        $pconfig['brightness']        = '-1';
+if (!isset($pconfig['offbrightness']))     $pconfig['offbrightness']     = '-1';
+if (!isset($pconfig['contrast']))          $pconfig['contrast']          = '-1';
+if (!isset($pconfig['backlight']))         $pconfig['backlight']         = 'default';
+if (!isset($pconfig['outputleds']))        $pconfig['outputleds']        = 'no';
+
+
+if ($_POST) {
+	unset($input_errors);
+	$pconfig = $_POST;
+
+	if (!$input_errors) {
+		$lcdproc_config['enabled']           = $pconfig['enabled'];
+		$lcdproc_config['enabled']           = $pconfig['enabled'];
+		$lcdproc_config['size']              = $pconfig['size'];   
+		$lcdproc_config['driver']            = $pconfig['driver'];
+		$lcdproc_config['connection_type']   = $pconfig['connection_type'];
+		$lcdproc_config['refresh_frequency'] = $pconfig['refresh_frequency'];
+		$lcdproc_config['port_speed']        = $pconfig['port_speed'];
+		$lcdproc_config['brightness']        = $pconfig['brightness'];
+		$lcdproc_config['offbrightness']     = $pconfig['offbrightness'];
+		$lcdproc_config['contrast']          = $pconfig['contrast'];
+		$lcdproc_config['backlight']         = $pconfig['backlight'];
+		$lcdproc_config['outputleds']        = $pconfig['outputleds'];
+				
+		write_config();
+		sync_package_lcdproc();
 	}
-}*/
+}
+
 
 $pgtitle = array(gettext("Services"), gettext("LCDproc"), gettext("Server"));
 include("head.inc");
 
 $tab_array = array();
-$tab_array[] = array(gettext("Server"), true, "/packages/lcdproc/lcdproc.php");
+$tab_array[] = array(gettext("Server"),  true,  "/packages/lcdproc/lcdproc.php");
 $tab_array[] = array(gettext("Screens"), false, "/packages/lcdproc/lcdproc_screens.php");
 display_top_tabs($tab_array);
 
 // The constructor for Form automatically creates a submit button. If you want to suppress that
 // use Form(false), of specify a different button using Form($mybutton)
 $form = new Form();
+$section = new Form_Section('LCD Options');
 
-// Create a new form section and give it a title
-$section = new Form_Section('General Options');
-
-// Add a checkbox to our new form section
+// Add the Enable checkbox
 $section->addInput(
 	new Form_Checkbox(
 		'enable', // checkbox name (id)
@@ -72,8 +99,7 @@ $section->addInput(
 	)
 );
 
-// <default_value>ucom1</default_value>
-// Add a selector (option) box passing the selector values in an associative array
+// Add the com port selector
 $section->addInput(
 	new Form_Select(
 		'comport',
@@ -96,9 +122,241 @@ $section->addInput(
 	)
 )->setHelp('Set the com port LCDproc should use.');
 
+$section->addInput(
+	new Form_Select(
+		'size',
+		'Display Size',
+		$pconfig['size'], // Initial value.
+		[
+			'12x1' => '1 rows 12 colums',
+			'12x2' => '2 rows 12 colums',
+			'12x4' => '4 rows 12 colums',
+			'16x1' => '1 row 16 colums',
+			'16x2' => '2 rows 16 colums',
+			'16x4' => '4 rows 16 colums',
+			'20x1' => '1 row 20 colums',
+			'20x2' => '2 rows 20 colums',
+			'20x4' => '4 rows 20 colums'
+		]
+	)
+)->setHelp('Set the display size lcdproc should use.');
+
+$section->addInput(
+	new Form_Select(
+		'driver',
+		'Driver',
+		$pconfig['driver'], // Initial value.
+		[
+			'bayrad'       => 'bayrad',
+			'CFontz'       => 'CrystalFontz',
+			'CFontz633'    => 'CrystalFontz 633',
+			'CFontzPacket' => 'CrystalFontz Packet',
+			'curses'       => 'curses',
+			'CwLnx'        => 'CwLnx',
+			'ea65'         => 'ea65',
+			'EyeboxOne'    => 'EyeboxOne',
+			'glk'          => 'glk',
+			'hd44780'      => 'hd44780',
+			'icp_a106'     => 'icp_a106',
+			'IOWarrior'    => 'IOWarrior',
+			'lb216'        => 'lb216',
+			'lcdm001'      => 'lcdm001',
+			'lcterm'       => 'lcterm',
+			'MD8800'       => 'MD8800',
+			'ms6931'       => 'ms6931',
+			'mtc_s16209x'  => 'mtc_s16209x',
+			'MtxOrb'       => 'MtxOrb',
+			'nexcom'       => 'nexcom (x86 only)',
+			'NoritakeVFD'  => 'NoritakeVFD',
+			'picolcd'      => 'picolcd',
+			'pyramid'      => 'pyramid',
+			'sdeclcd'      => 'Watchguard Firebox with SDEC',
+			'sed1330'      => 'sed1330',
+			'sed1520'      => 'sed1520',
+			'serialPOS'    => 'serialPOS',
+			'serialVFD'    => 'serialVFD',
+			'shuttleVFD'   => 'shuttleVFD',
+			'sli'          => 'sli',
+			'stv5730'      => 'stv5730',
+			'SureElec'     => 'Sure Electronics',
+			't6963'        => 't6963',
+			'text'         => 'text',
+			'tyan'         => 'tyan'
+		]
+	)
+)->setHelp('Set the LCD driver LCDproc should use.');
+
+$section->addInput(
+	new Form_Select(
+		'connection_type',
+		'Connection Type',
+		$pconfig['connection_type'], // Initial value.
+		[
+			'4bit'          => '4bit wiring to parallel port',
+			'8bit'          => '8bit wiring to parallel port (lcdtime)',
+			'winamp'        => '8bit wiring winamp style to parallel port',
+			'serialLpt'     => 'Serial LPT wiring',
+			'picanlcd'      => 'PIC-an-LCD serial device',
+			'lcdserializer' => 'LCD serializer',
+			'los-panel'     => 'LCD on serial panel device',
+			'vdr-lcd'       => 'VDR LCD serial device',
+			'vdr-wakeup'    => 'VDR-Wakeup module',
+			'pertelian'     => 'Pertelian X2040 LCD',
+			'bwctusb'       => 'BWCT USB device',
+			'lcd2usb'       => 'Till Harbaum\'s LCD2USB',
+			'usbtiny'       => 'Dick Streefland\'s USBtiny',
+			'lis2'          => 'LIS2 from VLSystem',
+			'mplay'         => 'MPlay Blast from VLSystem',
+			'ftdi'          => 'LCD connected to FTDI 2232D USB chip',
+			'usblcd'        => 'USBLCD adapter from Adams IT Services',
+			'i2c'           => 'LCD driven by PCF8574/PCA9554 connected via i2c'
+		]
+	)
+)->setHelp('Set connection type for the HD44780 driver');
+
+$section->addInput(
+	new Form_Select(
+		'refresh_frequency',
+		'Refresh frequency',
+		$pconfig['refresh_frequency'], // Initial value.
+		[
+			'1'  => '1 second',
+			'2'  => '2 seconds',
+			'3'  => '3 seconds',
+			'5'  => '5 seconds',
+			'10' => '10 seconds',
+			'15' => '15 seconds'
+		]
+	)
+)->setHelp('Set the refresh frequency of the information on the LCD Panel');
+
+$section->addInput(
+	new Form_Select(
+		'port_speed',
+		'Port speed',
+		$pconfig['port_speed'], // Initial value.
+		[
+			'0'      => 'Default',
+			'1200'   => '1200 bps',
+			'2400'   => '2400 bps',
+			'9600'   => '9600 bps',
+			'19200'  => '19200 bps',
+			'57600'  => '57600 bps',
+			'115200' => '115200 bps'
+		]
+	)
+)->setHelp('Set the port speed.<br />Caution: not all the driver or panels support all the speeds, leave "default" if unsure.');
+
+$section->addInput(
+	new Form_Select(
+		'brightness',
+		'Brightness',
+		$pconfig['brightness'], // Initial value.
+		[
+			'-1' => 'Default',
+			'0'  => '0%',
+			'10' => '10%',
+			'20' => '20%',
+			'30' => '30%',
+			'40' => '40%',
+			'50' => '50%',
+			'60' => '60%',
+			'70' => '70%',
+			'80' => '80%',
+			'90' => '90%',
+			'100' => '100%'
+		]
+	)
+)->setHelp('Set the brightness of the LCD panel.<br />This option is not supported by all the LCD panels, leave "default" if unsure.');
+
+$section->addInput(
+	new Form_Select(
+		'offbrightness',
+		'Off brightness',
+		$pconfig['offbrightness'], // Initial value.
+		[
+			'-1' => 'Default',
+			'0'  => '0%',
+			'10' => '10%',
+			'20' => '20%',
+			'30' => '30%',
+			'40' => '40%',
+			'50' => '50%',
+			'60' => '60%',
+			'70' => '70%',
+			'80' => '80%',
+			'90' => '90%',
+			'100' => '100%'
+		]
+	)
+)->setHelp(
+	'Set the off-brightness of the LCD panel. This value is used when the display is normally switched off in case LCDd is inactive.<br />' .
+	'This option is not supported by all the LCD panels, leave "default" if unsure.'
+);
+
+$section->addInput(
+	new Form_Select(
+		'contrast',
+		'Contrast',
+		$pconfig['contrast'], // Initial value.
+		[
+			'-1' => 'Default',
+			'0'  => '0%',
+			'10' => '10%',
+			'20' => '20%',
+			'30' => '30%',
+			'40' => '40%',
+			'50' => '50%',
+			'60' => '60%',
+			'70' => '70%',
+			'80' => '80%',
+			'90' => '90%',
+			'100' => '100%'
+		]
+	)
+)->setHelp(
+	'Set the contrast of the LCD panel.<br />' .
+	'This option is not supported by all the LCD panels, leave "default" if unsure.'
+);
+
+$section->addInput(
+	new Form_Select(
+		'backlight',
+		'Backlight',
+		$pconfig['backlight'], // Initial value.
+		[
+			'default' => 'Default',
+			'on'      => 'On',
+			'off'     => 'Off'
+		]
+	)
+)->setHelp(
+	'Set the backlight setting. If set to the default value, then the backlight setting of the display can be influenced by the clients.<br />' .
+	'This option is not supported by all the LCD panels, leave "default" if unsure.'
+);
+
+$section->addInput(
+	new Form_Checkbox(
+		'outputleds', // checkbox name (id)
+		'Enable Output LEDs', // checkbox label
+		'Enable the Output LEDs present on some LCD panels.', // checkbox text
+		$pconfig['outputleds'] // checkbox initial value
+	)
+)->setHelp(
+	'This feature is currently supported by the CFontz633 driver only.<br />' .
+	'Each LED can be off or show two colors: RED (alarm) or GREEN (everything ok) and shows:<br />' .
+	'LED1: NICs status (green: ok, red: at least one nic down)<br />' .
+	'LED2: CARP status (green: master, red: backup, off: CARP not implemented)<br />' .
+	'LED3: CPU status (green &lt; 50%, red &gt; 50%)<br />' .
+	'LED4: Gateway status (green: ok, red: at least one gateway not responding, off: no gateway configured).'
+);
+
 
 $form->add($section); // Add the section to our form
+print($form); // Finally . . We can display our new form
+
 ?>
+
 <div class="infoblock">
 	<?=print_info_box('For more information see: <a href="http://lcdproc.org/docs.php3">LCDproc documentation</a>.', info)?>
 </div>
