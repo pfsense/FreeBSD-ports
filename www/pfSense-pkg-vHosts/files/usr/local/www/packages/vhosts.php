4bfb2cf1607fd792acb6cf1eeeafade9411e77ff
Steve Thames <sthames42@gmail.com>
2016-11-11 18:10:06 -0800

    All working. Need to add config section.

--- www/pfSense-pkg-vHosts/files/usr/local/www/packages/vhosts.php
+++ www/pfSense-pkg-vHosts/files/usr/local/www/packages/vhosts.php
@@ -16,6 +16,9 @@
 * GET: 'act' = 'del', 'id' = n
 *   Removes host 'n' config, update web config, and drops list page.
 *
+* GET: 'act' = 'tog', 'id' = n
+*	Toggles Disabled flag for host 'n'.
+*
 * POST: 'id' = n (optional)
 *   Adds or updates host config, updates web config, and drops list page.
 * ------------------------------------------------------------------------------
@@ -29,27 +32,48 @@
 *
 * License: https://github.com/pfsense/pfsense/blob/master/LICENSE
 *******************************************************************************/
-require("guiconfig.inc");
+require_once("guiconfig.inc");
+require_once("util.inc");
 require("/usr/local/pkg/vhosts.inc");
 
+/*******************************************************************************
+* save_config_and_exit */
+/**
+* Writes out the configuration, updates the config files, and reloads page.
+*******************************************************************************/
+function save_config_and_exit()
+	{
+	write_config();
+	vhosts_sync_package();
+	mark_subsystem_dirty('vhosts');
+	header("Location: ?");
+	exit;
+	}	
+
+/*******************************************************************************
+* Main Line
+*******************************************************************************/
 $a_vhosts  = &$config['installedpackages']['vhosts']['config'];
 $a_cert    =  $config['cert'];
 $act       = $_GET['act'];
 $id        = ($_POST ? $_POST['id']  : $_GET['id']);
 $good_id   = (isset($id) && $a_vhosts[$id]);
+$me        = basename(__FILE__);
 
 /*---------------------------------------------------------*/
 /* Add/Update - Validate input and save changes to config. */
 /*---------------------------------------------------------*/
 if ($_POST)
 	{
-	$pconfig['disabled']    = $_POST['disabled'];
-	$pconfig['name']        = $_POST['name'];
-	$pconfig['ipaddress']   = $_POST['ipaddress'];
-	$pconfig['port']        = $_POST['port'];
-	$pconfig['hostname']    = $_POST['hostname'];
-	$pconfig['certref']     = $_POST['certref'];
-	$pconfig['description'] = $_POST['description'];
+	$pconfig['name']        = trim($_POST['name']);
+	$pconfig['ipaddress']   =      $_POST['ipaddress'];
+	$pconfig['port']        =      $_POST['port'];
+	$pconfig['hostname']    = trim($_POST['hostname']);
+	$pconfig['certref']     =      $_POST['certref'];
+	$pconfig['description'] = trim($_POST['description']);
+	
+	if ($_POST['disabled'] == 'yes')
+		$pconfig['disabled'] = true;
     
 	/*---------------------------------------*/
 	/* Validate the input and update config. */
@@ -72,8 +96,7 @@ if ($_POST)
 		else
 			$a_vhosts[]    = $pconfig;
 			
-		write_config();
-		vhosts_sync_package();
+		save_config_and_exit();	
 		}
 			
 	/*-------------------------------------------------*/
@@ -82,6 +105,19 @@ if ($_POST)
 	else
 		$input_action = ($good_id ? 'Edit' : 'Add');
 	}
+
+/*--------------------------*/
+/* Toggle enabled/disabled. */
+/*--------------------------*/
+elseif ($good_id && $act == 'tog')
+	{
+	if (isset($a_vhosts[$id]['disabled']))
+		unset($a_vhosts[$id]['disabled']);
+	else
+		$a_vhosts[$id]['disabled'] = true;
+
+	save_config_and_exit();
+	}
 		
 /*--------------------------------------*/
 /* Delete - Remove vhost configuration. */
@@ -89,8 +125,7 @@ if ($_POST)
 elseif ($good_id && $act == 'del')
 	{	
 	unset($a_vhosts[$id]);
-	write_config();
-	vhosts_sync_package();
+	save_config_and_exit();
 	}
 	
 /*----------------------------------------------*/
@@ -100,7 +135,7 @@ elseif ($good_id && $act == 'chg')
 	{
 	$input_action = 'Edit';
 	
-	$pconfig['disabled']    = $a_vhosts[$id]['disabled'];
+	$pconfig['disabled']    = isset($a_vhosts[$id]['disabled']);
 	$pconfig['name']        = $a_vhosts[$id]['name'];
 	$pconfig['ipaddress']   = $a_vhosts[$id]['ipaddress'];
 	$pconfig['port']        = $a_vhosts[$id]['port'];
@@ -167,7 +202,7 @@ if (isset($input_action))
 	$section->addInput(new Form_Input    ('port',        'Port', 'number', $pconfig['port'], ['min' => '0']))
 		->setHelp('Required. Make sure the IP and Port combination does not conflict with the local system.');
 		
-	$section->addInput(new Form_Input    ('host',        'Host Name', 'text', $pconfig['host']))
+	$section->addInput(new Form_Input    ('hostname',    'Host Name', 'text', $pconfig['hostname']))
 		->setHelp('Name in Host Header for Name-Based Host. Not required for IP-Based host.');
 		 
 	$section->addInput(new Form_Select   ('certref',	'Server certificate', $pconfig['certref'], $certlist))
@@ -192,13 +227,11 @@ else
 	
 	include("head.inc");
 	
+	if (is_subsystem_dirty('vhosts'))
+		print_info_box(gettext('vHosts configuration has been changed. (Re)start service to apply changes.'));
+	
 	?>
 	<div class="panel panel-default">
-			<?php print_info_box(gettext('vHosts is a web server package that can host HTML, Javascript, CSS, and PHP. It creates another instance of the '.
-											'nginx web server that is already installed. It uses PHP5 in FastCGI mode and has access to PHP Data Objects '.
-											'and PDO SQLite. To use SFTP, enable SSH from <b>System->Advanced->Enable Secure Shell</b>. Then SFTP can be used '.
-											'to access the files at <b>/usr/local/vhosts</b>. After adding or updating an entry make sure to restart the '.
-											'<a href="/status_services.php">service</a> to apply the settings.'), 'info', false); ?>
 		<div class="panel-heading"></div>
 		<div class="panel-body table-responsive">
 			<table class="table table-striped table-hover table-condensed sortable-theme-bootstrap table-rowdblclickedit" data-sortable>
@@ -218,18 +251,29 @@ else
 				for ($i=0; $i < count($a_vhosts); $i++)
 					{
 					$vhost     = $a_vhosts[$i];
-					$disabled  = $vhost['disabled'];
+					$disabled  = isset($vhost['disabled']);
 					$name      = $vhost['name'];
 					$hostname  = $vhost['hostname'];
 					$ipaddress = $vhost['ipaddress'];
 					$port      = $vhost['port'];
-					$htmlname  = htmlspecialchars($name);
 					$ssl       = lookup_cert($vhost['certref']);
-					$hosturl   = ($ssl ? 'https' : 'http')."://".($hostname ? explode($hostname, ' ')[0]: "$ipaddress:$port");
+					
+					/*-------------------------------------------------*/
+					/* Build link to vhost around host name. Use first */
+					/* host listed in 'hostname' or 'ipaddress:port'.  */
+					/*-------------------------------------------------*/
+					$htmlname  = htmlspecialchars($name);
+					$hosturl   = ($ssl ? 'https' : 'http')."://".($hostname ? explode(' ', $hostname)[0] : $ipaddress).":$port";
 					$hostlink  = ($disabled ? $htmlname : "<a href='$hosturl'>$htmlname</a>");
 					
+					if ($disabled)
+						{
+						$toggle_text  = gettext('Enable');
+						$toggle_class = 'fa fa-check-square-o';
+						}
+					
 					?>
-					<tr <?=isset($disabled) ? 'class="disabled"':''?>>
+					<tr <?=$disabled ? 'class="disabled"':''?>>
 						<td><?=$hostlink?></td>
 						<td><?=htmlspecialchars($vhost['ipaddress'])?></td>
 						<td><?=htmlspecialchars($vhost['port'])?></td>
@@ -237,8 +281,13 @@ else
 						<td><?=($ssl ? 'Yes' : 'No')?></td>
 						<td><?=htmlspecialchars($vhost['description'])?></td>
 						<td>
-							<a class="fa fa-pencil"	title="<?=gettext('Edit host')?>"   href="vhosts.php?act=chg&amp;id=<?=$i?>"></a>
-							<a class="fa fa-trash"	title="<?=gettext('Delete host')?>" href="vhosts.php?act=del&amp;id=<?=$i?>"></a>
+							<a class="fa fa-pencil"	title="<?=gettext('Edit')?>"   href="?act=chg&amp;id=<?=$i?>"></a>
+							
+							<a class="fa fa-<?=($disabled ? 'check-square-o' : 'ban')?>" 
+							   title="<?=gettext($disabled ? "Enable" : "Disable")?>" 
+							    href="?act=tog&amp;id=<?=$i?>"></a>
+							    
+							<a class="fa fa-trash"	title="<?=gettext('Delete this host')?>" href="?act=del&amp;id=<?=$i?>"></a>
 						</td>
 					</tr>
 					<?php
@@ -250,11 +299,21 @@ else
 	</div>
 	
 	<nav class="action-buttons">
-		<a href="vhosts.php?act=add" class="btn btn-sm btn-success btn-sm">
+		<a href="?act=add" class="btn btn-sm btn-success btn-sm">
 		<i class="fa fa-plus icon-embed-btn"></i>
 			<?=gettext("Add")?>
 		</a>
 	</nav>
+	
+	<div class="infoblock">
+		<div class="alert alert-info clearfix" role="alert">
+			<?=gettext('vHosts is a web server package that can host HTML, Javascript, CSS, and PHP. It creates another instance of the '.
+						'nginx web server that is already installed. It uses PHP5 in FastCGI mode and has access to PHP Data Objects '.
+						'and PDO SQLite. To use SFTP, enable SSH from <b>System->Advanced->Enable Secure Shell</b>. Then SFTP can be used '.
+						'to access the files at <b>/usr/local/vhosts</b>. After adding or updating an entry make sure to restart the '.
+						'<a href="/status_services.php">service</a> to apply the settings.'); ?>
+		</div>
+	</div>
 	
 	<?php
 	}
