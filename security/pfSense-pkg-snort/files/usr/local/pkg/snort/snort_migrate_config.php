6bde6a4c953adbaf1ca87ec125adbc3dcbb3196c
Demair Ramos <demair.fapitec@gmail.com>
2016-12-12 05:01:28 +0300

    Restore license and copyright here

--- security/pfSense-pkg-snort/files/usr/local/pkg/snort/snort_migrate_config.php
+++ security/pfSense-pkg-snort/files/usr/local/pkg/snort/snort_migrate_config.php
@@ -2,15 +2,27 @@
 /*
  * snort_migrate_config.php
  *
+ * part of pfSense (https://www.pfsense.org)
+ * Copyright (c) 2006-2016 Rubicon Communications, LLC (Netgate)
  * Copyright (C) 2013, 2014 Bill Meeks
+ * 
  * All rights reserved.
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 1. Redistributions of source code must retain the above copyright notice,
  * this list of conditions and the following disclaimer.
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * 2. Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
