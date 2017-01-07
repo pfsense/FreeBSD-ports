56adca230fadc29251f4462f6b154d4d43f343f2
Charlie Root <root@pfsense-builder.themeeks.net>
2017-01-07 13:26:39 -0500

    Explicity check for "off" on option default so empty values default properly.

--- security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_global.php
+++ security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_global.php
@@ -50,7 +50,7 @@ else {
 	$pconfig['forcekeepsettings'] = $config['installedpackages']['suricata']['config'][0]['forcekeepsettings'] == "on" ? 'on' : 'off';
 	$pconfig['snortcommunityrules'] = $config['installedpackages']['suricata']['config'][0]['snortcommunityrules'] == "on" ? 'on' : 'off';
 	$pconfig['snort_rules_file'] = $config['installedpackages']['suricata']['config'][0]['snort_rules_file'];
-	$pconfig['autogeoipupdate'] = $config['installedpackages']['suricata']['config'][0]['autogeoipupdate'] == "on" ? 'on' : 'off';
+	$pconfig['autogeoipupdate'] = $config['installedpackages']['suricata']['config'][0]['autogeoipupdate'] == "off" ? 'off' : 'on';
 	$pconfig['hide_deprecated_rules'] = $config['installedpackages']['suricata']['config'][0]['hide_deprecated_rules'] == "on" ? 'on' : 'off';
 }
 
