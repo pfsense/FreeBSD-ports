d6aeec01d958f3a25b37a737326e59bfe2ee42d7
Renato Botelho <garga@FreeBSD.org>
2017-01-27 16:27:59 -0200

    Do not clear entries for CMD_TYPE when wake threads. It should help #7143

--- net/filterdns/files/filterdns.c
+++ net/filterdns/files/filterdns.c
@@ -971,7 +971,8 @@ merge_config(void *arg __unused) {
 						    "Waking resolving thread for host %s",
 						    thr->hostname);
 					tmpthr->exit = 2;
-					filterdns_clean_table(tmpthr, 1);
+					if (tmpthr->type != CMD_TYPE)
+						filterdns_clean_table(tmpthr, 1);
 					pthread_mutex_lock(&tmpthr->mtx);
 					pthread_cond_signal(&tmpthr->cond);
 					pthread_mutex_unlock(&tmpthr->mtx);
