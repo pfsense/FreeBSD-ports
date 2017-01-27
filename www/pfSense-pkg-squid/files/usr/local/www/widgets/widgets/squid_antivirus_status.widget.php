b4ac137b56e0c395737c0b2d7020e9f62937e5d6
doktornotor <notordoktor@gmail.com>
2017-01-27 22:41:32 +0100

    Squid - dashboard widget bootstrap facelift + fixes
    - replace inline base64 images with bootstrap icons
    - tidy up tables and remove old styling
    - fix squiclamav version detection and use pkg_exec() to get packages' versions in general
    (cherry picked from commit 83187fc68caf9da33bcc1851ebec7e8d1836a666)

--- www/pfSense-pkg-squid/files/usr/local/www/widgets/widgets/squid_antivirus_status.widget.php
+++ www/pfSense-pkg-squid/files/usr/local/www/widgets/widgets/squid_antivirus_status.widget.php
@@ -1,51 +1,44 @@
 <?php
 /*
-	squid_antivirus_status.widget.php
-	part of pfSense (https://www.pfSense.org/)
-	Copyright (C) 2010 Serg Dvoriancev <dv_serg@mail.ru>
-	Copyright (C) 2015 ESF, LLC
-	All rights reserved.
+ * squid_antivirus_status.widget.php
+ *
+ * part of pfSense (https://www.pfsense.org)
+ * Copyright (c) 2015-2017 Rubicon Communications, LLC (Netgate)
+ * Copyright (C) 2010 Serg Dvoriancev <dv_serg@mail.ru>
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-
-	1. Redistributions of source code must retain the above copyright notice,
-	   this list of conditions and the following disclaimer.
-
-	2. Redistributions in binary form must reproduce the above copyright
-	   notice, this list of conditions and the following disclaimer in the
-	   documentation and/or other materials provided with the distribution.
-
-	THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
-	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-	AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-	OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
-*/
+require_once("functions.inc");
 require_once("guiconfig.inc");
 require_once("pfsense-utils.inc");
-require_once("functions.inc");
 require_once("pkg-utils.inc");
+require_once("service-utils.inc");
 if (file_exists("/usr/local/pkg/squid.inc")) {
 	require_once("/usr/local/pkg/squid.inc");
 } else {
-	echo "No squid.inc found. You must have Squid3 package installed to use this widget.";
+	echo "No squid.inc found. You must have Squid package installed to use this widget.";
 }
 
 define('PATH_CLAMDB', '/var/db/clamav');
 define('PATH_SQUID', SQUID_BASE . '/bin/squid');
 define('PATH_AVLOG', '/var/log/c-icap/virus.log');
-global $clamd_path, $cicap_cfg_path, $img;
+global $clamd_path, $img;
 $clamd_path = SQUID_BASE . "/sbin/clamd";
-$cicap_cfg_path = SQUID_LOCALBASE . "/bin/c-icap-config";
 $img = array();
-$img['up'] = "<img src='data:image/gif;base64,R0lGODlhCwALAIABACPcMP///yH+FUNyZWF0ZWQgd2l0aCBUaGUgR0lNUAAh+QQBCgABACwAAAAACwALAAACFYwNpwi50eKK9NA722Puyf15GjgaBQA7' title='Service running' alt='' />";
-$img['down'] = "<img src='data:image/gif;base64,R0lGODlhCwALAIABANwjI////yH+FUNyZWF0ZWQgd2l0aCBUaGUgR0lNUAAh+QQBCgABACwAAAAACwALAAACFowDeYvKlsCD7sXZ5Iq89kpdFshoRwEAOw==' title='Service not running' alt='' />";
+$img['up'] = '<i class="fa fa-level-up text-success" title="Service running"></i>';
+$img['down'] = '<i class="fa fa-level-down text-danger" title="Service not running"></i>';
 
 function squid_avdb_info($filename) {
 	$stl = "style='padding-top: 0px; padding-bottom: 0px; padding-left: 4px; padding-right: 4px; border-left: 1px solid #999999;'";
@@ -56,11 +49,11 @@ function squid_avdb_info($filename) {
 		if ($handle = fopen($path, "r")) {
 			$s = fread($handle, 1024);
 			$s = explode(':', $s);
-			# datetime
+			// datetime
 			$dt = explode(" ", $s[1]);
 			$s[1] = strftime("%Y.%m.%d", strtotime("{$dt[0]} {$dt[1]} {$dt[2]}"));
 			if ($s[0] == 'ClamAV-VDB') {
-				$r .= "<tr class='listr'><td>{$filename}</td><td {$stl}>{$s[1]}</td><td {$stl}>{$s[2]}</td><td $stl>{$s[7]}</td></tr>";
+				$r .= "<tr><td>{$filename}</td><td {$stl}>{$s[1]}</td><td {$stl}>{$s[2]}</td><td $stl>{$s[7]}</td></tr>";
 			}
 			fclose($handle);
 		}
@@ -69,8 +62,8 @@ function squid_avdb_info($filename) {
 }
 
 function squid_antivirus_bases_info() {
-	$db = '<table width="100%" border="0" cellspacing="0" cellpadding="1"><tbody>';
-	$db .= '<tr class="vncellt" ><td>Database</td><td>Date</td><td>Version</td><td>Builder</td></tr>';
+	$db = '<table class="table table-striped table-hover table-condensed"><tbody>';
+	$db .= '<tr><th>Database</th><th>Date</th><th>Version</th><th>Builder</th></tr>';
 	$avdbs = array("daily.cvd", "daily.cld", "bytecode.cvd", "bytecode.cld", "main.cvd", "main.cld", "safebrowsing.cvd", "safebrowsing.cld");
 	foreach ($avdbs as $avdb) {
 		$db .= squid_avdb_info($avdb);
@@ -80,27 +73,19 @@ function squid_antivirus_bases_info() {
 }
 
 function squid_clamav_version() {
-	global $clamd_path, $cicap_cfg_path, $img;
-	if (is_executable($clamd_path)) {
-		$s = (is_service_running("clamd") ? $img['up'] : $img['down']);
-		$version = preg_split("@/@", shell_exec("{$clamd_path} -V"));
-		$s .= "&nbsp;&nbsp;{$version[0]}";
-	} else {
-		$s .= "&nbsp;&nbsp;ClamAV: N/A";
-	}
-	if (is_executable($cicap_cfg_path)) {
-		$s .= "&nbsp;&nbsp;";
-		$s .= (is_service_running("c-icap") ? $img['up'] : $img['down']);
-		$s .= "&nbsp;&nbsp;C-ICAP " . shell_exec("{$cicap_cfg_path} --version");
-	} else {
-		$s .= "&nbsp;&nbsp;C-ICAP: N/A";
-	}
-	if (file_exists("/usr/local/www/squid_clwarn.php")) {
-		preg_match("@(VERSION.*).(\d{1}).(\d{2})@", file_get_contents("/usr/local/www/squid_clwarn.php"), $squidclamav_version);
-		$s .= "+&nbsp;&nbsp;SquidClamav " . str_replace("'", "", strstr($squidclamav_version[0], "'"));
-	} else {
-		$s .= "+&nbsp;&nbsp;SquidClamav: N/A";
-	}
+	global $img;
+	// ClamAV status and version
+	$s = (is_service_running("clamd") ? $img['up'] : $img['down']);
+	pkg_exec("query '%v' clamav", $version, $err);
+	$s .= "&nbsp;&nbsp;ClamAV {$version}";
+	$s .= "&nbsp;&nbsp;";
+	// C-ICAP status and version
+	$s .= (is_service_running("c-icap") ? $img['up'] : $img['down']);
+	pkg_exec("query '%v' c-icap", $version, $err);
+	$s .= "&nbsp;&nbsp;C-ICAP {$version}";
+	// SquidClamav version
+	pkg_exec("query '%v' squidclamav", $version, $err);
+	$s .= "+&nbsp;&nbsp;SquidClamav {$version}";
 	return $s;
 }
 
@@ -126,11 +111,12 @@ function squid_antivirus_statistics() {
 
 ?>
 
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<div class="table-responsive">
+	<table class="table table-striped table-hover table-condensed">
 	<tbody>
 		<tr>
-			<td class="vncellt">Squid Version</td>
-			<td class="listr" width="75%">
+			<th>Squid Version</th>
+			<td width="75%">
 			<?php
 				$updown = (is_service_running("squid") ? $img['up'] : $img['down']);
 				pkg_exec("query '%v' squid", $version, $err);
@@ -139,28 +125,29 @@ function squid_antivirus_statistics() {
 			</td>
 		</tr>
 		<tr>
-			<td class="vncellt">Antivirus Scanner</td>
-			<td class="listr" width="75%">
+			<th>Antivirus Scanner</th>
+			<td width="75%">
 				<?php echo squid_clamav_version(); ?>
 			</td>
 		</tr>
 		<tr>
-			<td class="vncellt">Antivirus Bases</td>
-			<td class="listr" width="75%">
+			<th style="vertical-align:middle;">Antivirus Bases</th>
+			<td width="75%">
 				<?php echo squid_antivirus_bases_info(); ?>
 			</td>
 		</tr>
 		<tr>
-			<td class="vncellt">Last Update</td>
-			<td class="listr" width="75%">
+			<th>Last Update</th>
+			<td width="75%">
 				<?php echo squid_avupdate_status(); ?>
 			</td>
 		</tr>
 		<tr>
-			<td class="vncellt">Statistics</td>
-			<td class="listr" width="75%">
+			<th>Statistics</th>
+			<td width="75%">
 				<?php echo squid_antivirus_statistics(); ?>
 			</td>
 		</tr>
 	</tbody>
-</table>
+	</table>
+</div>
