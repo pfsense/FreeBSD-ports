73246541879f9256f4241b2a22dc61e6e31e6bd2
jim-p <jimp@pfsense.org>
2017-02-03 13:50:45 -0500

    Adjust nsupdate code to account for multiple entries that work individually. Fixes #7192

--- security/pfSense-pkg-acme/files/usr/local/pkg/acme/dnsapi/dns_nsupdate.sh
+++ security/pfSense-pkg-acme/files/usr/local/pkg/acme/dnsapi/dns_nsupdate.sh
@@ -6,14 +6,19 @@
 dns_nsupdate_add() {
   fulldomain=$1
   txtvalue=$2
-  _checkKeyFile || return 1
-  [ -n "${NSUPDATE_SERVER}" ] || NSUPDATE_SERVER="localhost"
+  _checkKeyFile $fulldomain || return 1
+  THISNSUPDATE_KEY="${NSUPDATE_KEY}${fulldomain}.key"
+  if [ ! -r "${NSUPDATE_SERVER}${fulldomain}.server" ] || [ -z "${NSUPDATE_SERVER}" ]; then
+    THISNSUPDATE_SERVER="localhost"
+  else
+    THISNSUPDATE_SERVER=`cat "${NSUPDATE_SERVER}${fulldomain}.server"`
+  fi
   # save the dns server and key to the account conf file.
-  _saveaccountconf NSUPDATE_SERVER "${NSUPDATE_SERVER}"
-  _saveaccountconf NSUPDATE_KEY "${NSUPDATE_KEY}"
+  _saveaccountconf NSUPDATE_SERVER "${THISNSUPDATE_SERVER}"
+  _saveaccountconf NSUPDATE_KEY "${THISNSUPDATE_KEY}"
   _info "adding ${fulldomain}. 60 in txt \"${txtvalue}\""
-  nsupdate -k "${NSUPDATE_KEY}" <<EOF
-server ${NSUPDATE_SERVER}
+  nsupdate -k "${THISNSUPDATE_KEY}" <<EOF
+server ${THISNSUPDATE_SERVER}
 update add ${fulldomain}. 60 in txt "${txtvalue}"
 send
 EOF
@@ -28,11 +33,17 @@ EOF
 #Usage: dns_nsupdate_rm   _acme-challenge.www.domain.com
 dns_nsupdate_rm() {
   fulldomain=$1
-  _checkKeyFile || return 1
-  [ -n "${NSUPDATE_SERVER}" ] || NSUPDATE_SERVER="localhost"
+  test "${fulldomain#*_acme-challenge}" == "${fulldomain}" && _info "Skipping nsupdate for TXT on base domain." && return 0
+  if [ ! -r "${NSUPDATE_SERVER}${fulldomain}.server" ] || [ -z "${NSUPDATE_SERVER}" ]; then
+    THISNSUPDATE_SERVER="localhost"
+  else
+    THISNSUPDATE_SERVER=`cat "${NSUPDATE_SERVER}${fulldomain}.server"`
+  fi
+  _checkKeyFile $fulldomain || return 1
+  THISNSUPDATE_KEY="${NSUPDATE_KEY}${fulldomain}.key"
   _info "removing ${fulldomain}. txt"
-  nsupdate -k "${NSUPDATE_KEY}" <<EOF
-server ${NSUPDATE_SERVER}
+  nsupdate -k "${THISNSUPDATE_KEY}" <<EOF
+server ${THISNSUPDATE_SERVER}
 update delete ${fulldomain}. txt
 send
 EOF
@@ -47,12 +58,13 @@ EOF
 ####################  Private functions below ##################################
 
 _checkKeyFile() {
-  if [ -z "${NSUPDATE_KEY}" ]; then
+  THISNSUPDATE_KEY="${NSUPDATE_KEY}${1}.key"
+  if [ -z "${THISNSUPDATE_KEY}" ]; then
     _err "you must specify a path to the nsupdate key file"
     return 1
   fi
-  if [ ! -r "${NSUPDATE_KEY}" ]; then
-    _err "key ${NSUPDATE_KEY} is unreadable"
+  if [ ! -r "${THISNSUPDATE_KEY}" ]; then
+    _err "key ${THISNSUPDATE_KEY} is unreadable"
     return 1
   fi
 }
