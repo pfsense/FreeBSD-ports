ef694000da79b0d80b7bf4735379cfccb100357b
Phil Davis <phil.davis@inf.org>
2017-02-13 11:54:44 +0545

    System Patches 'Behavior' typo (cherry picked from commit 5aed4e2af05958f1bdc241aeecc8161a7efc2b06)

--- sysutils/pfSense-pkg-System_Patches/files/usr/local/www/system_patches_edit.php
+++ sysutils/pfSense-pkg-System_Patches/files/usr/local/www/system_patches_edit.php
@@ -197,7 +197,7 @@ $section->addInput($patchtext);
 
 $form->add($section);
 
-$section = new Form_Section('Patch Application Behavoir');
+$section = new Form_Section('Patch Application Behavior');
 
 $section->addInput(new Form_Select(
 	'pathstrip',
