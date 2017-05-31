f2938f7a21434f9454290a6b0154fc00b3a01a18
Julian <julian@net23.de>
2017-05-31 20:35:19 +0200

    Including 'configpassthru' in the yaml template
    Also fixing a form render error: After saving the "Advanced Configuration Pass-Through" is showing base64 encoded text.

--- security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_interfaces_edit.php
+++ security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_interfaces_edit.php
@@ -1054,7 +1054,7 @@ $section = new Form_Section('Arguments here will be automatically inserted into 
 $section->addInput(new Form_Textarea (
 	'configpassthru',
 	'Advanced Configuration Pass-Through',
-	$pconfig['configpassthru']
+	($_POST['configpassthru']?$_POST['configpassthru']:$pconfig['configpassthru'])
 ))->setHelp('Enter any additional configuration parameters to add to the Suricata configuration here, separated by a newline');
 
 $form->add($section);
