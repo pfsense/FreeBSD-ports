f69569a1f5423d2013068716f9b61a84acfa5aaa
Renato Botelho <garga@FreeBSD.org>
2017-10-09 09:59:08 -0300

    Fix bad style introduced in f299308978

--- devel/php56-pfSense-module/files/pfSense.c
+++ devel/php56-pfSense-module/files/pfSense.c
@@ -4003,7 +4003,7 @@ static void build_ipsec_sa_array(void *salist, char *label, vici_res_t *res) {
 	/* message sections may be nested. maintain a stack as we traverse */
 
 	int done = 0;
-  int level = 0;
+	int level = 0;
 	zval *nestedarrs[32];
 	nestedarrs[level] = (zval *) salist;
 
@@ -4016,14 +4016,14 @@ static void build_ipsec_sa_array(void *salist, char *label, vici_res_t *res) {
 				name = vici_parse_name(res);
 				ALLOC_INIT_ZVAL(nestedarrs[level + 1]);
 				array_init(nestedarrs[level + 1]);
-        if(level == 0){
-            add_next_index_zval(nestedarrs[level],nestedarrs[level+1]);
+				if (level == 0) {
+					add_next_index_zval(nestedarrs[level],nestedarrs[level+1]);
 
-            char *temp = "con-id";
-            add_assoc_string(nestedarrs[level + 1], temp, name, 1);
-        }else{
-				    add_assoc_zval(nestedarrs[level], name, nestedarrs[level + 1]);
-        }
+					char *temp = "con-id";
+					add_assoc_string(nestedarrs[level + 1], temp, name, 1);
+				} else {
+					add_assoc_zval(nestedarrs[level], name, nestedarrs[level + 1]);
+				}
 				Z_ADDREF_P(nestedarrs[level + 1]);
 				level++;
 				break;
@@ -4053,11 +4053,11 @@ static void build_ipsec_sa_array(void *salist, char *label, vici_res_t *res) {
 				add_next_index_string(nestedarrs[level], value, 1);
 				break;
 			case VICI_PARSE_END:
-				done ++;
+				done++;
 				break;
 			default:
 				php_printf("Parse error!\n");
-				done ++;
+				done++;
 				break;
 		}
 	}
@@ -4083,7 +4083,6 @@ PHP_FUNCTION(pfSense_ipsec_list_sa) {
 			if (res) {
 				vici_free_res(res);
 			}
-    
 		}
 		vici_disconnect(conn);
 	} else {
