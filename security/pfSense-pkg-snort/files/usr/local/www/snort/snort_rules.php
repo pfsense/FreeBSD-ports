e4bd97c343674e2ea59573d0e8a3bdbb4e07c9be
doktornotor <notordoktor@gmail.com>
2017-10-21 21:41:46 +0200

    Remove useless conf_mount_{ro,rw} calls

--- security/pfSense-pkg-snort/files/usr/local/www/snort/snort_rules.php
+++ security/pfSense-pkg-snort/files/usr/local/www/snort/snort_rules.php
@@ -440,9 +440,7 @@ elseif (isset($_POST['clear'])) {
 	unset($a_rule[$id]['customrules']);
 	write_config("Snort pkg: clear all custom rules for {$a_rule[$id]['interface']}.");
 	$rebuild_rules = true;
-	conf_mount_rw();
 	snort_generate_conf($a_rule[$id]);
-	conf_mount_ro();
 	$rebuild_rules = false;
 	$pconfig['customrules'] = '';
 
@@ -457,9 +455,7 @@ elseif (isset($_POST['save'])) {
 		unset($a_rule[$id]['customrules']);
 	write_config("Snort pkg: save modified custom rules for {$a_rule[$id]['interface']}.");
 	$rebuild_rules = true;
-	conf_mount_rw();
 	snort_generate_conf($a_rule[$id]);
-	conf_mount_ro();
 	$rebuild_rules = false;
 	$output = "";
 	$retcode = "";
@@ -492,9 +488,7 @@ elseif ($_POST['apply']) {
 	/* rules for this interface.                     */
 	/*************************************************/
 	$rebuild_rules = true;
-	conf_mount_rw();
 	snort_generate_conf($a_rule[$id]);
-	conf_mount_ro();
 	$rebuild_rules = false;
 
 	// Soft-restart Snort to live-load new rules
