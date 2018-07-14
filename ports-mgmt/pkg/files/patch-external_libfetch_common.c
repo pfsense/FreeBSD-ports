0f74fb19456a705e59612be57942370baf52d7f7
Luiz Souza <luiz@netgate.com>
2018-07-13 23:42:55 -0300

    On static binaries, initiliaze OpenSSL only once during the lifetime of the process.

--- ports-mgmt/pkg/files/patch-external_libfetch_common.c
+++ ports-mgmt/pkg/files/patch-external_libfetch_common.c
@@ -1,17 +1,28 @@
 --- external/libfetch/common.c.orig	2017-08-17 03:56:56 UTC
 +++ external/libfetch/common.c
-@@ -60,6 +60,10 @@
+@@ -60,6 +60,11 @@
  #define INFTIM (-1)
  #endif
  
 +#ifdef WITH_STATIC_ENGINE
 +void ENGINE_load_ateccx08(void);
++static int fetch_ssl_initilized = 0;
 +#endif
 +
  /*** Local data **************************************************************/
  
  /*
-@@ -842,7 +846,11 @@ fetch_ssl(conn_t *conn, const struct url
+@@ -836,13 +841,26 @@ fetch_ssl(conn_t *conn, const struct url
+ 	X509_NAME *name;
+ 	char *str;
+ 
++#ifdef WITH_STATIC_ENGINE
++	if (fetch_ssl_initilized == 0) {
++#endif
++
+ 	/* Init the SSL library and context */
+ 	if (!SSL_library_init()){
+ 		fprintf(stderr, "SSL library init failed\n");
  		return (-1);
  	}
  
@@ -20,6 +31,11 @@
 +#endif
  	SSL_load_error_strings();
 +	OPENSSL_config(NULL);
++
++#ifdef WITH_STATIC_ENGINE
++		fetch_ssl_initilized = 1;
++	}
++#endif
  
  	conn->ssl_meth = SSLv23_client_method();
  	conn->ssl_ctx = SSL_CTX_new(conn->ssl_meth);
