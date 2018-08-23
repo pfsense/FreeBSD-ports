3a09d464b0b18ecc5a449ecb038415f28a99a28b
Tim Econ <tim@economid.es>
2018-08-23 14:29:37 -0400

    Adding OSPF Cost Shifting for HA failover

--- net/pfSense-pkg-Quagga_OSPF/files/usr/local/www/status_ospfd.php
+++ net/pfSense-pkg-Quagga_OSPF/files/usr/local/www/status_ospfd.php
@@ -2,6 +2,7 @@
 /*
  * status_ospfd.php
  *
+ *
  * part of pfSense (https://www.pfsense.org)
  * Copyright (c) 2010-2015 Rubicon Communications, LLC (Netgate)
  * Copyright (C) 2010 Nick Buraglio <nick@buraglio.com>
