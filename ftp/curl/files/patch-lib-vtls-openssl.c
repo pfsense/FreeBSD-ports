6d12ddadbfe0a22e13c960fb120bd26cd8fec8ca
Luiz Souza <luiz@netgate.com>
2018-10-16 18:00:38 -0300

    Fix the OpenSSL engine support in curl.
    Obtained from:	https://github.com/curl/curl/issues/3023
    
    (cherry picked from commit 3c0078ef0cc26bb931d9f7160ed2e37b200ff15c)

--- ftp/curl/files/patch-lib-vtls-openssl.c
+++ ftp/curl/files/patch-lib-vtls-openssl.c
@@ -1,6 +1,15 @@
---- lib/vtls/openssl.c.orig	2018-03-22 14:26:38 UTC
+--- lib/vtls/openssl.c.orig	2018-09-04 20:46:08 UTC
 +++ lib/vtls/openssl.c
-@@ -117,12 +117,7 @@
+@@ -69,7 +69,7 @@
+ #include <openssl/ocsp.h>
+ #endif
+ 
+-#if (OPENSSL_VERSION_NUMBER >= 0x10000000L) && /* 1.0.0 or later */     \
++#if (OPENSSL_VERSION_NUMBER >= 0x0090800fL) && /* 0.9.8 or later */     \
+   !defined(OPENSSL_NO_ENGINE)
+ #define USE_OPENSSL_ENGINE
+ #include <openssl/engine.h>
+@@ -129,12 +129,7 @@
  #define X509_get0_notBefore(x) X509_get_notBefore(x)
  #define X509_get0_notAfter(x) X509_get_notAfter(x)
  #define CONST_EXTS /* nope */
@@ -14,7 +23,16 @@
  #define OpenSSL_version_num() SSLeay()
  #endif
  #endif
-@@ -3526,7 +3521,11 @@ static size_t Curl_ossl_version(char *bu
+@@ -978,7 +973,7 @@ static int Curl_ossl_init(void)
+ 
+   OPENSSL_load_builtin_modules();
+ 
+-#ifdef HAVE_ENGINE_LOAD_BUILTIN_ENGINES
++#ifdef USE_OPENSSL_ENGINE
+   ENGINE_load_builtin_engines();
+ #endif
+ 
+@@ -3698,7 +3693,11 @@ static size_t Curl_ossl_version(char *bu
    unsigned long ssleay_value;
    sub[2]='\0';
    sub[1]='\0';
