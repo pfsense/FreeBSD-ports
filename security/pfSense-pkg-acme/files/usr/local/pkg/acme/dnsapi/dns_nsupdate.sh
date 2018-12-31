362190028d84ca36c7b025d747014390f234d3c1
jim-p <jimp@netgate.com>
2018-12-31 14:19:03 -0500

    ACME 0.5 update
    * TLS-ALPN support, replaces old TLS-SNI support, binds to a local port (e.g. 443), similar to standalone for HTTP on port 80. If you must use this, bind to an alternate port and forward WAN:443 to the alternate port to avoid a conflict with the GUI web server.
    * New alternate certificate authority: BuyPass (experimental), to be used instead of Let's Encrypt. This CA has different policies than LE, for example the certificates are valid for 180 days instead of 90, and has a different chain of trust. It still uses the ACME protocol.
    * New providers
    
      * Exoscale
      * Linode v4 API (split from existing Linode option code)
    
    * Various bug fixes from acme.sh upstream.
    
    BuyPass support added by https://github.com/mysteq in ports PR #607 but
    manually added to this commit instead.
    
    (cherry picked from commit d4183e87a3df280cb76afa4e21c6371d54e636ab)

--- security/pfSense-pkg-acme/files/usr/local/pkg/acme/dnsapi/dns_nsupdate.sh
+++ security/pfSense-pkg-acme/files/usr/local/pkg/acme/dnsapi/dns_nsupdate.sh
@@ -16,14 +16,26 @@ dns_nsupdate_add() {
   [ -n "${NSUPDATE_SERVER_PORT}" ] || NSUPDATE_SERVER_PORT=53
   # save the dns server and key to the account conf file.
   _saveaccountconf NSUPDATE_SERVER "${THISNSUPDATE_SERVER}"
-  _saveaccountconf NSUPDATE_SERVER_PORT "${NSUPDATE_SERVER_PORT}"
+  _saveaccountconf NSUPDATE_SERVER_PORT "${THISNSUPDATE_SERVER_PORT}"
   _saveaccountconf NSUPDATE_KEY "${THISNSUPDATE_KEY}"
+  _saveaccountconf NSUPDATE_ZONE "${THISNSUPDATE_ZONE}"
   _info "adding ${fulldomain}. 60 in txt \"${txtvalue}\""
-  nsupdate -k "${THISNSUPDATE_KEY}" <<EOF
-server ${THISNSUPDATE_SERVER} ${NSUPDATE_SERVER_PORT}
+  [ -n "$DEBUG" ] && [ "$DEBUG" -ge "$DEBUG_LEVEL_1" ] && nsdebug="-d"
+  [ -n "$DEBUG" ] && [ "$DEBUG" -ge "$DEBUG_LEVEL_2" ] && nsdebug="-D"
+  if [ -z "${THISNSUPDATE_ZONE}" ]; then
+    nsupdate -k "${THISNSUPDATE_KEY}" $nsdebug <<EOF
+server ${THISNSUPDATE_SERVER}  ${NSUPDATE_SERVER_PORT} 
 update add ${fulldomain}. 60 in txt "${txtvalue}"
 send
 EOF
+  else
+    nsupdate -k "${THISNSUPDATE_KEY}" $nsdebug <<EOF
+server ${THISNSUPDATE_SERVER}  ${NSUPDATE_SERVER_PORT}
+zone ${THISNSUPDATE_ZONE}.
+update add ${fulldomain}. 60 in txt "${txtvalue}"
+send
+EOF
+  fi
   if [ $? -ne 0 ]; then
     _err "error updating domain"
     return 1
@@ -35,6 +47,7 @@ EOF
 #Usage: dns_nsupdate_rm   _acme-challenge.www.domain.com
 dns_nsupdate_rm() {
   fulldomain=$1
+  _checkKeyFile || return 1
   test "${fulldomain#*_acme-challenge}" == "${fulldomain}" && _info "Skipping nsupdate for TXT on base domain." && return 0
   if [ ! -r "${NSUPDATE_SERVER}${fulldomain}.server" ] || [ -z "${NSUPDATE_SERVER}" ]; then
     THISNSUPDATE_SERVER="localhost"
@@ -45,11 +58,22 @@ dns_nsupdate_rm() {
   THISNSUPDATE_KEY="${NSUPDATE_KEY}${fulldomain}.key"
   [ -n "${NSUPDATE_SERVER_PORT}" ] || NSUPDATE_SERVER_PORT=53
   _info "removing ${fulldomain}. txt"
-  nsupdate -k "${THISNSUPDATE_KEY}" <<EOF
-server ${THISNSUPDATE_SERVER} ${NSUPDATE_SERVER_PORT}
+  [ -n "$DEBUG" ] && [ "$DEBUG" -ge "$DEBUG_LEVEL_1" ] && nsdebug="-d"
+  [ -n "$DEBUG" ] && [ "$DEBUG" -ge "$DEBUG_LEVEL_2" ] && nsdebug="-D"
+  if [ -z "${THISNSUPDATE_ZONE}" ]; then
+    nsupdate -k "${THISNSUPDATE_KEY}" $nsdebug <<EOF
+server ${THISNSUPDATE_SERVER}  ${NSUPDATE_SERVER_PORT} 
 update delete ${fulldomain}. txt
 send
 EOF
+  else
+    nsupdate -k "${THISNSUPDATE_KEY}" $nsdebug <<EOF
+server ${THISNSUPDATE_SERVER}  ${NSUPDATE_SERVER_PORT}
+zone ${THISNSUPDATE_ZONE}.
+update delete ${fulldomain}. txt
+send
+EOF
+  fi
   if [ $? -ne 0 ]; then
     _err "error updating domain"
     return 1
