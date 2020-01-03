167d721807eeef708c5f3264cfb3273895413c36
jim-p <jimp@netgate.com>
2020-01-03 12:26:23 -0500

    This is 2020. Issue #9245

--- net/pfSense-pkg-ipsec-profile-wizard/files/usr/local/www/vpn_ipsec_profile.php
+++ net/pfSense-pkg-ipsec-profile-wizard/files/usr/local/www/vpn_ipsec_profile.php
@@ -3,7 +3,7 @@
  * vpn_ipsec_profile.php
  *
  * part of pfSense (https://www.pfsense.org)
- * Copyright (c) 2016 Rubicon Communications, LLC (Netgate)
+ * Copyright (c) 2016-2020 Rubicon Communications, LLC (Netgate)
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
