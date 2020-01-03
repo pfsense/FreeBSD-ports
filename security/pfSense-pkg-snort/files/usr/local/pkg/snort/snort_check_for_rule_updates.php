447e2fdab7a07f4b829bc7bf9ef48485a708262d
jim-p <jimp@netgate.com>
2020-01-03 11:41:58 -0500

    This is 2020. Issue #9245

--- security/pfSense-pkg-snort/files/usr/local/pkg/snort/snort_check_for_rule_updates.php
+++ security/pfSense-pkg-snort/files/usr/local/pkg/snort/snort_check_for_rule_updates.php
@@ -3,7 +3,7 @@
  * snort_check_for_rule_updates.php
  *
  * part of pfSense (https://www.pfsense.org)
- * Copyright (c) 2006-2019 Rubicon Communications, LLC (Netgate)
+ * Copyright (c) 2006-2020 Rubicon Communications, LLC (Netgate)
  * Copyright (c) 2009 Robert Zelaya
  * Copyright (c) 2013-2019 Bill Meeks
  * All rights reserved.
--- security/pfSense-pkg-snort/files/usr/local/www/snort/snort_rulesets.php
+++ security/pfSense-pkg-snort/files/usr/local/www/snort/snort_rulesets.php
@@ -3,7 +3,7 @@
  * snort_rulesets.php
  *
  * part of pfSense (https://www.pfsense.org)
- * Copyright (c) 2006-2016 Rubicon Communications, LLC (Netgate)
+ * Copyright (c) 2006-2020 Rubicon Communications, LLC (Netgate)
  * Copyright (c) 2009 Robert Zelaya
  * Copyright (c) 2018 Bill Meeks
  * All rights reserved.
