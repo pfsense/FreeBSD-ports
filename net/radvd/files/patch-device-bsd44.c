238247ce5f93de2cbc8b3dde5313c5998d49a8c0
Luiz Otavio O Souza <luiz@netgate.com>
2020-01-22 12:10:48 -0200

    Silence the useless warning about IPv6 disabled forwarding in FreeBSD:
    "IPv6 forwarding on interface seems to be disabled, but continuing anyway"
    
    Ticket: #10195, #9577 (partially)
    (cherry picked from commit 853f675257eb5c7993b5773a581251a4b7bc6490)

--- net/radvd/files/patch-device-bsd44.c
+++ net/radvd/files/patch-device-bsd44.c
@@ -1,6 +1,6 @@
---- device-bsd44.c.orig	2017-06-29 04:32:29 UTC
+--- device-bsd44.c.orig	2018-02-18 22:45:02 UTC
 +++ device-bsd44.c
-@@ -126,7 +126,31 @@ ret:
+@@ -126,8 +126,32 @@ ret:
  	return -1;
  }
  
@@ -8,7 +8,7 @@
 +int setup_allrouters_membership(int sock, struct Interface *iface) 
 +{
 +	struct ipv6_mreq mreq;
-+
+ 
 +	memset(&mreq, 0, sizeof(mreq));
 +	mreq.ipv6mr_interface = iface->props.if_index;
 +
@@ -30,6 +30,14 @@
 +
 +	return 0; 
 +}
- 
++
  int set_interface_linkmtu(const char *iface, uint32_t mtu)
  {
+ 	dlog(LOG_DEBUG, 4, "setting LinkMTU (%u) for %s is not supported", mtu, iface);
+@@ -161,5 +185,5 @@ int check_ip6_forwarding(void)
+ int check_ip6_iface_forwarding(const char *iface)
+ {
+ 	dlog(LOG_DEBUG, 4, "checking ipv6 forwarding of interface not supported");
+-	return -1;
++	return 0;
+ }
