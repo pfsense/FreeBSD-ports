ca039b812ec217cfad08f7812ac1dc4e3174a640
Renato Botelho <garga@FreeBSD.org>
2020-05-22 14:02:47 -0300

    Require flashrom 1.1 and remove broken parameter

--- sysutils/pfSense-pkg-Netgate_Coreboot_Upgrade/files/adi_flash_util/adi_flash_util.c
+++ sysutils/pfSense-pkg-Netgate_Coreboot_Upgrade/files/adi_flash_util/adi_flash_util.c
@@ -38,8 +38,8 @@
 const char version[] = "01.00.00.01";
 
 static char read_flash_cmd[] = "flashrom -p internal -r " FLASH_FILENAME " >/dev/null 2>&1";
-static char write_full_flash_cmd[] = "flashrom -p internal:ich_spi_force=yes -w " FLASH_FILENAME " >/dev/null 2>&1";
-static char write_4kflash_cmd[] = "flashrom -p internal:ich_spi_force=yes -l " BIOS_LAYOUT " -i 4kdescriptor -w " FLASH_FILENAME " >/dev/null 2>&1";
+static char write_full_flash_cmd[] = "flashrom -p internal -w " FLASH_FILENAME " >/dev/null 2>&1";
+static char write_4kflash_cmd[] = "flashrom -p internal -l " BIOS_LAYOUT " -i 4kdescriptor -w " FLASH_FILENAME " >/dev/null 2>&1";
 
 int flash_size = ADI_RCCVE_FLASH_SIZE;
 
