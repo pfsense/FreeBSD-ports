2c85bc9839f3af42ccc470cee627247b7ef6469d
Shadonet <ingprs01@gmail.com>
2020-08-31 21:00:16 +0100

    Add a missing license and copyright part

--- security/pfSense-pkg-zeek/files/usr/local/www/select_box_file.php
+++ security/pfSense-pkg-zeek/files/usr/local/www/select_box_file.php
@@ -1,4 +1,24 @@
 <?php
+/*
+* select_box_file.php
+* part of pfSense (https://www.pfSense.org/)
+* Copyright (c) 2018-2020 Prosper Doko
+* Copyright (c) 2020 Mark Overholser
+* All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 // list_by_ext: returns an array containing an alphabetic list of files in the specified directory ($path) with a file extension that matches $extension
 function list_by_ext($extension, $path) {
 	$list = array();
