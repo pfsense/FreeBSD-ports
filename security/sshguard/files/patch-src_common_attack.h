956578a94074ff97ea8bdf5be3a4ca31db7f5471
Renato Botelho do Couto <renato@netgate.com>
2020-09-21 12:12:59 -0500

    Regenerate sshguard patches

--- security/sshguard/files/patch-src_common_attack.h
+++ security/sshguard/files/patch-src_common_attack.h
@@ -1,9 +1,9 @@
---- src/common/attack.h.orig	2019-05-24 16:16:43 UTC
+--- src/common/attack.h.orig	2020-06-23 23:01:26 UTC
 +++ src/common/attack.h
 @@ -45,6 +45,7 @@ enum service {
      SERVICES_CLF_UNAUTH     = 350,  //< HTTP 401 in common log format
      SERVICES_CLF_PROBES     = 360,  //< probes for common web services
-     SERVICES_CLF_WORDPRESS  = 370,  //< WordPress logins in common log format
+     SERVICES_CLF_LOGIN_URL  = 370,  //< CMS framework logins in common log format
 +    SERVICES_PFSENSE        = 380,  //< pfSense UI login
      SERVICES_OPENVPN        = 400,  //< OpenVPN
      SERVICES_GITEA          = 500,  //< Gitea
