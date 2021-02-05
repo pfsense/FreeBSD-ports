fe7a03898d7cf8d14fd895519b0962c1b26fcb0e
Renato Botelho do Couto <renato@netgate.com>
2021-02-05 14:57:36 -0300

    Revert "Downgrade lang/rust to 1.44.1 to make it possible to build on aarch64"
    This reverts commit 00abe7f8aa45a656426791d9c810f6ad96fdfd45.

--- lang/rust/files/patch-vendor_openssl-sys_build_main.rs
+++ lang/rust/files/patch-vendor_openssl-sys_build_main.rs
@@ -1,21 +1,19 @@
---- vendor/openssl-sys/build/main.rs.orig	2020-06-01 17:45:25 UTC
-+++ vendor/openssl-sys/build/main.rs
-@@ -204,6 +204,9 @@ See rust-openssl README for more information:
-             (3, 0, 0) => ('3', '0', '0'),
-             (3, 0, 1) => ('3', '0', '1'),
-             (3, 0, _) => ('3', '0', 'x'),
-+            (3, 1, 0) => ('3', '1', '0'),
-+            (3, 1, _) => ('3', '1', 'x'),
-+            (3, 2, 0) => ('3', '2', '0'),
+--- vendor/openssl-sys/build/main.rs.orig	2020-11-16 06:27:50.000000000 -0800
++++ vendor/openssl-sys/build/main.rs	2020-12-11 12:47:11.685331000 -0800
+@@ -221,6 +221,7 @@
+             (3, 1, 0) => ('3', '1', '0'),
+             (3, 1, _) => ('3', '1', 'x'),
+             (3, 2, 0) => ('3', '2', '0'),
++            (3, 2, _) => ('3', '2', 'x'),
              _ => version_error(),
          };
  
-@@ -244,7 +247,7 @@ fn version_error() -> ! {
+@@ -261,7 +262,7 @@
          "
  
  This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
--through 3.0.x, but a different version of OpenSSL was found. The build is now aborting
-+through 3.2.0, but a different version of OpenSSL was found. The build is now aborting
+-through 3.2.0, but a different version of OpenSSL was found. The build is now aborting
++through 3.2.x, but a different version of OpenSSL was found. The build is now aborting
  due to this version mismatch.
  
  "
