cd1a02b69fccad41c94d141bd6243b1f2fef1368
Renato Botelho do Couto <renato@netgate.com>
2021-02-05 14:54:39 -0300

    Revert "Downgrade lang/rust to 1.44.1 to make it possible to build on aarch64"
    This reverts commit 00abe7f8aa45a656426791d9c810f6ad96fdfd45.

--- lang/rust/files/patch-src_bootstrap_native.rs
+++ lang/rust/files/patch-src_bootstrap_native.rs
@@ -1,11 +1,19 @@
-There seems to be some kind of race when using llvm-config-wrapper
-for building rust-lld.  Attempt to improve reliability of the build
-by not using it.  llvm-config-wrapper is a hack in the first place
-that is only really needed on Windows.
+From 9741fbd202b2b55de95abe1eb7f3d8185e312444 Mon Sep 17 00:00:00 2001
+From: Jake Goulding <jake.goulding@gmail.com>
+Date: Sat, 11 Jul 2020 09:38:01 -0400
+Subject: [PATCH] Don't allow `DESTDIR` to influence LLVM builds
 
---- src/bootstrap/native.rs.orig	2020-02-27 18:39:49 UTC
+When running a command like `DESTDIR=foo x.py install` in a completely
+clean build directory, this will cause LLVM to be installed into
+`DESTDIR`, which then causes the build to fail later when it attempts
+to *use* those LLVM files.
+---
+ src/bootstrap/native.rs | 5 +++++
+ 1 file changed, 5 insertions(+)
+
+--- src/bootstrap/native.rs.orig	2020-08-24 15:00:49 UTC
 +++ src/bootstrap/native.rs
-@@ -467,25 +467,9 @@ impl Step for Lld {
+@@ -517,26 +522,9 @@ impl Step for Lld {
          let mut cfg = cmake::Config::new(builder.src.join("src/llvm-project/lld"));
          configure_cmake(builder, target, &mut cfg, true);
  
@@ -24,11 +32,12 @@ that is only really needed on Windows.
 -        // can't build on a system where your paths require `\` on Windows, but
 -        // there's probably a lot of reasons you can't do that other than this.
 -        let llvm_config_shim = env::current_exe().unwrap().with_file_name("llvm-config-wrapper");
+-
          cfg.out_dir(&out_dir)
              .profile("Release")
--            .env("LLVM_CONFIG_REAL", llvm_config)
+-            .env("LLVM_CONFIG_REAL", &llvm_config)
 -            .define("LLVM_CONFIG_PATH", llvm_config_shim)
-+            .define("LLVM_CONFIG_PATH", llvm_config)
++            .define("LLVM_CONFIG_PATH", &llvm_config)
              .define("LLVM_INCLUDE_TESTS", "OFF");
  
-         cfg.build();
+         // While we're using this horrible workaround to shim the execution of
