4078221e713ea863f5856a22939f8c8620d3fff1
Bill Meeks <bmeeks8@bellsouth.net>
2021-02-11 14:51:39 -0500

    Support multiple custom IP addresses, subnets and aliases in Pass List.

--- security/pfSense-pkg-snort/files/usr/local/www/snort/snort_passlist.php
+++ security/pfSense-pkg-snort/files/usr/local/www/snort/snort_passlist.php
@@ -5,7 +5,7 @@
  * part of pfSense (https://www.pfsense.org)
  * Copyright (c) 2004-2021 Rubicon Communications, LLC (Netgate)
  * Copyright (c) 2009-2010 Robert Zelaya
- * Copyright (c) 2019 Bill Meeks
+ * Copyright (c) 2021 Bill Meeks
  * All rights reserved.
  *
  * originially part of m0n0wall (http://m0n0.ch/wall)
@@ -112,7 +112,8 @@ else {
 	}
 }
 
-$pgtitle = array(gettext("Services"), gettext("Snort"), gettext("Pass Lists"));
+$pglinks = array("", "/snort/snort_interfaces.php", "@self");
+$pgtitle = array("Services", "Snort", "Pass Lists");
 include_once("head.inc");
 
 /* Display Alert message */
@@ -150,7 +151,6 @@ display_top_tabs($tab_array, true);
 			<tr>
 				<th>&nbsp;</th>
 				<th>List Name</th>
-				<th>Assigned Alias</th>
 				<th>Description</th>
 				<th>Actions</th>
 			</tr>
@@ -160,13 +160,6 @@ display_top_tabs($tab_array, true);
 			<tr>
 				<td><input type="checkbox" id="frc<?=$i?>" name="del[]" value="<?=$i?>" onclick="fr_bgcolor('<?=$i?>')" /></td>
 				<td ondblclick="document.location='snort_passlist_edit.php?id=<?=$i;?>';"><?=htmlspecialchars($list['name']);?></td>
-		<?php if (strlen($list['address']) > 0) : ?>
-				<td ondblclick="document.location='snort_passlist_edit.php?id=<?=$i;?>';" title="<?=filter_expand_alias($list['address']);?>">
-					<?=gettext($list['address']);?></td>
-		<?php else : ?>
-				<td ondblclick="document.location='snort_passlist_edit.php?id=<?=$i;?>';">
-				</td>
-		<?php endif; ?>
 				<td ondblclick="document.location='snort_passlist_edit.php?id=<?=$i;?>';"><?=htmlspecialchars($list['descr']);?>&nbsp;</td>
 				<td style="cursor: pointer;"><a href="snort_passlist_edit.php?id=<?=$i;?>" class="fa fa-pencil" title="<?=gettext('Edit Pass List');?>"></a>
 				<a class="fa fa-trash no-confirm" id="Xcdel_<?=$i?>" title="<?=gettext('Delete Pass List'); ?>"></a>
@@ -178,12 +171,12 @@ display_top_tabs($tab_array, true);
 	</div>
 
 	<nav class="action-buttons">
-		<a href="snort_passlist_edit.php?id=<?php echo $id_gen;?>" role="button" class="btn btn-sm btn-success" title="<?=gettext('add a new pass list');?>">
+		<a href="snort_passlist_edit.php?id=<?php echo $id_gen;?>" role="button" class="btn btn-sm btn-success" title="<?=gettext('Add a new pass list');?>">
 			<i class="fa fa-plus icon-embed-btn"></i>
 			<?=gettext("Add");?>
 		</a>
 		<?php if (count($a_passlist) > 0): ?>
-			<button type="submit" name="del_btn" id="del_btn" class="btn btn-danger btn-sm" title="<?=gettext('Delete Selected Items');?>">
+			<button type="submit" name="del_btn" id="del_btn" class="btn btn-danger btn-sm" title="<?=gettext('Delete selected items');?>">
 				<i class="fa fa-trash icon-embed-btn"></i>
 				<?=gettext('Delete');?>
 			</button>
