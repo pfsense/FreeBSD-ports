4bf659ea23f402ba29b06b457cb11bb6e98d08ec
Brad Davis <bdavis@netgate.com>
2022-09-15 18:43:35 +0000

    Import a build fix on main from upstream net-snmp

--- net-mgmt/net-snmp/files/patch-agent_mibgroup_ip-mib_data__access_scalars__sysctl.c
+++ net-mgmt/net-snmp/files/patch-agent_mibgroup_ip-mib_data__access_scalars__sysctl.c
@@ -1,27 +1,45 @@
 --- agent/mibgroup/ip-mib/data_access/scalars_sysctl.c.orig	2021-05-25 22:19:35 UTC
 +++ agent/mibgroup/ip-mib/data_access/scalars_sysctl.c
-@@ -274,6 +274,16 @@ netsnmp_arch_ip_scalars_register_handlers(void)
- {
-     static oid ipReasmTimeout_oid[] = { 1, 3, 6, 1, 2, 1, 4, 13, 0 };
+@@ -267,13 +267,20 @@ netsnmp_arch_ip_scalars_ipv6IpForwarding_set(u_long va
+     return 0;
+ }
  
+-static long ipReasmTimeout_val;
+-
+-void
+-netsnmp_arch_ip_scalars_register_handlers(void)
++long netsnmp_arch_ip_scalars_ipReasmTimeout_get(void)
+ {
+-    static oid ipReasmTimeout_oid[] = { 1, 3, 6, 1, 2, 1, 4, 13, 0 };
 +#ifdef freebsd14
 +#define	FRAGTTL_CTL	"net.inet.ip.fragttl"
 +    int intval;
-+
-+    if (sysctlbyname(FRAGTTL_CTL, &intval, &(size_t){sizeof(int)}, NULL, 0) < 0)
+ 
++    if (sysctlbyname(FRAGTTL_CTL, &intval, &(size_t){sizeof(int)}, NULL, 0) < 0) {
 +        DEBUGMSGTL(("access::ipReasmTimeout", "sysctl %s failed - %s\n",
 +                    FRAGTTL_CTL,
 +                    strerror(errno)));
-+    ipReasmTimeout_val = intval;
++		return -1;
++	}
++	return intval;
 +#else
      /* 
       * This value is static at compile time on FreeBSD; it really should be a
       * probed via either sysctl or sysconf at runtime as the compiled value and
-@@ -283,6 +293,7 @@ netsnmp_arch_ip_scalars_register_handlers(void)
+@@ -282,10 +289,11 @@ netsnmp_arch_ip_scalars_register_handlers(void)
+      * Please refer to sys/protosw.h for more details on what this value is (in
       * particular PR_SLOWHZ).
       */
-     ipReasmTimeout_val = IPFRAGTTL / PR_SLOWHZ;
+-    ipReasmTimeout_val = IPFRAGTTL / PR_SLOWHZ;
++    return IPFRAGTTL / PR_SLOWHZ;
 +#endif
++}
  
-     netsnmp_register_long_instance("ipReasmTimeout",
-                                    ipReasmTimeout_oid,
+-    netsnmp_register_long_instance("ipReasmTimeout",
+-                                   ipReasmTimeout_oid,
+-                                   OID_LENGTH(ipReasmTimeout_oid),
+-                                   &ipReasmTimeout_val, NULL);
++void
++netsnmp_arch_ip_scalars_register_handlers(void)
++{
+ }
