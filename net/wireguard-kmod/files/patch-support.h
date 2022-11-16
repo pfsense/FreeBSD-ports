ba95ebf8117d66ff43a817497a34a76a08dd4bf8
Christian McDonald <cmcdonald@netgate.com>
2022-11-16 11:16:32 -0500

    net/wireguard-kmod: sync with upstream

--- net/wireguard-kmod/files/patch-support.h
+++ net/wireguard-kmod/files/patch-support.h
@@ -9,9 +9,15 @@ load") changed the way this function should be invoked.
 Link: https://github.com/freebsd/freebsd-src/commit/e7d02be19d40063783d6b8f1ff2bc4c7170fd434
 Reported-by: Michael Pro <michael.adm@gmail.com>
 Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
---- support.h.orig	2022-06-15 14:11:06 UTC
+---
+ src/support.h | 4 ++++
+ 1 file changed, 4 insertions(+)
+
+diff --git a/src/support.h b/src/support.h
+index d1c326c..ee6b03c 100644
+--- support.h
 +++ support.h
-@@ -37,7 +37,11 @@ sogetsockaddr(struct socket *so, struct sockaddr **nam
+@@ -37,7 +37,11 @@ sogetsockaddr(struct socket *so, struct sockaddr **nam)
  	int error;
  
  	CURVNET_SET(so->so_vnet);
@@ -23,3 +29,6 @@ Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
  	CURVNET_RESTORE();
  	return (error);
  }
+-- 
+cgit v1.2.3-11-g984f
+
