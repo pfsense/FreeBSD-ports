c6becd7fcdd9ca9536ffdd5eae4e68c9d9a83086
Bill Meeks <bmeeks8@bellsouth.net>
2023-02-20 14:04:59 -0500

    Fix Redmine #13995 - Suricata not saving auto-assigned Suppress List.

--- security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_alerts.php
+++ security/pfSense-pkg-suricata/files/usr/local/www/suricata/suricata_alerts.php
@@ -67,25 +67,25 @@ function suricata_add_supplist_entry($suppress) {
 	/*   TRUE if successful or FALSE on failure     */
 	/************************************************/
 
-	global $a_instance, $instanceid;
+	global $instanceid;
 
 	$a_suppress = config_get_path('installedpackages/suricata/suppress/item', []);
 	$found_list = false;
 
 	/* If no Suppress List is set for the interface, then create one with the interface name */
-	if (empty($a_instance['suppresslistname']) || $a_instance['suppresslistname'] == 'default') {
+	if (empty(config_get_path("installedpackages/suricata/rule/{$instanceid}/suppresslistname", '')) || config_get_path("installedpackages/suricata/rule/{$instanceid}/suppresslistname", '') == 'default') {
 		$s_list = array();
 		$s_list['uuid'] = uniqid();
-		$s_list['name'] = $a_instance['interface'] . "suppress" . "_" . $s_list['uuid'];
+		$s_list['name'] = config_get_path("installedpackages/suricata/rule/{$instanceid}/interface") . "suppress" . "_" . $s_list['uuid'];
 		$s_list['descr']  =  "Auto-generated list for Alert suppression";
 		$s_list['suppresspassthru'] = base64_encode($suppress);
 		$a_suppress[] = $s_list;
-		$a_instance['suppresslistname'] = $s_list['name'];
+		config_set_path("installedpackages/suricata/rule/{$instanceid}/suppresslistname", $s_list['name']);
 		$found_list = true;
 	} else {
 		/* If we get here, a Suppress List is defined for the interface so see if we can find it */
 		foreach ($a_suppress as $a_id => $alist) {
-			if ($alist['name'] == $a_instance['suppresslistname']) {
+			if ($alist['name'] == config_get_path("installedpackages/suricata/rule/{$instanceid}/suppresslistname", '')) {
 				$found_list = true;
 				if (!empty($alist['suppresspassthru'])) {
 					$tmplist = base64_decode($alist['suppresspassthru']);
@@ -104,7 +104,6 @@ function suricata_add_supplist_entry($suppress) {
 	/* If we created a new list or updated an existing one, save the change */
 	/* and return true; otherwise return false.                             */
 	if ($found_list) {
-		config_set_path("installedpackages/suricata/rule/{$instanceid}", $a_instance);
 		config_set_path('installedpackages/suricata/suppress/item', $a_suppress);
 		write_config("Suricata pkg: saved change to Suppress List " . $s_list['name'] . " from ALERTS tab.");
 		sync_suricata_package_config();
