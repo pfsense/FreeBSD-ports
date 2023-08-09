18cebbf0414a6ba725a8c808824ff671bdbfe842
jim-p <jimp@netgate.com>
2023-08-09 09:07:29 -0400

    Update haproxy_global.php to fix #14445
    For -devel package
    
    From yobyot @ GitHub
    
    (cherry picked from commit 1673063cd4ef8ca423d63e6b020431738837df03)

--- net/pfSense-pkg-haproxy-devel/files/usr/local/www/haproxy/haproxy_global.php
+++ net/pfSense-pkg-haproxy-devel/files/usr/local/www/haproxy/haproxy_global.php
@@ -130,13 +130,12 @@ if ($_POST) {
 			}
 
 			// flag for Status/Services to show when the package is 'disabled' so no start button is shown.
-			init_config_arr(array('installedpackages', 'haproxy', 'config', 0));
 			if ($_POST['enable']) {
-				if (is_array($haproxycfg['config'][0])) {
-					unset($haproxycfg['config'][0]['enable']);
+				if (array_get_path($haproxycfg, 'config/0/enable')) {
+					array_del_path($haproxycfg, 'config/0/enable');
 				}
 			} else {
-				$haproxycfg['config'][0]['enable'] = 'off';
+				array_set_path($haproxycfg, 'config/0/enable', 'off');
 			}
 			
 			touch($d_haproxyconfdirty_path);
