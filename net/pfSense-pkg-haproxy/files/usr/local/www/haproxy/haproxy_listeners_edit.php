9459b79854245fa0b9bbfc94515d9a0934f8544b
jim-p <jimp@netgate.com>
2023-08-10 08:50:43 -0400

    Catch haproxy up with -devel code. Fixes #14674
    (cherry picked from commit 9f7d258917ece10b6d55435776d2db85370e289c)

--- net/pfSense-pkg-haproxy/files/usr/local/www/haproxy/haproxy_listeners_edit.php
+++ net/pfSense-pkg-haproxy/files/usr/local/www/haproxy/haproxy_listeners_edit.php
@@ -46,7 +46,7 @@ $a_pools = getarraybyref($config,'installedpackages','haproxy','ha_pools','item'
 uasort($a_pools, 'haproxy_compareByName');
 
 global $simplefields;
-$simplefields = array('name','desc','status','secondary','primary_frontend','type','forwardfor','httpclose','extaddr','backend_serverpool',
+$simplefields = array('name','descr','status','secondary','primary_frontend','type','forwardfor','httpclose','extaddr','backend_serverpool',
 	'max_connections','client_timeout','port','advanced_bind',
 	'ssloffloadcert','sslsnifilter','ssl_crtlist_advanced','dcertadv','ssloffload','ssloffloadacl','ssloffloadacl_an','ssloffloadacladditional','ssloffloadacladditional_an',
 	'sslclientcert-none','sslclientcert-invalid','sslocsp',
@@ -359,7 +359,7 @@ if ($_POST) {
 			}
 		}
 
-		if ($_POST['client_timeout'] !== "" && !is_numeric($_POST['client_timeout'])) {
+		if (!empty($_POST['client_timeout']) && !is_numeric($_POST['client_timeout'])) {
 			$input_errors[] = sprintf(gettext("The value '%s' in field 'Client timeout' is not a number."), htmlspecialchars($_POST['client_timeout']));
 		}
 	}
@@ -625,7 +625,7 @@ if ($pconfig['status'] == 'disable') {
 }
 
 $section->addInput(new Form_Input('name', 'Name', 'text', $pconfig['name']));
-$section->addInput(new Form_Input('desc', 'Description', 'text', $pconfig['desc']));
+$section->addInput(new Form_Input('descr', 'Description', 'text', $pconfig['descr']));
 
 $section->addInput(new Form_Select(
 	'status',
@@ -717,7 +717,7 @@ $section->addInput(new Form_StaticText(
 	</table>
 	<br/>
 	acl's with the same name will be 'combined' using OR criteria.<br/>
-	For more information about ACL's please see <a href='http://cbonte.github.io/haproxy-dconv/1.7/configuration.html#7' target='_blank'>HAProxy Documentation</a> Section 7 - Using ACL's<br/><br/>
+	For more information about ACL's please see <a href='http://cbonte.github.io/haproxy-dconv/2.4/configuration.html#7' target='_blank'>HAProxy Documentation</a> Section 7 - Using ACL's<br/><br/>
 	<strong>NOTE Important change in behaviour, since package version 0.32</strong><br/>
 	-acl's are no longer combined with logical AND operators, list multiple acl's below where needed.<br/>
 	-acl's alone no longer implicitly generate use_backend configuration. Add 'actions' below to accomplish this behaviour.
@@ -995,7 +995,13 @@ var port_array  = <?= json_encode(get_alias_list(array("port", "url_ports", "url
 var address_array = <?= json_encode(get_alias_list(array("host", "network", "openvpn", "urltable"))) ?>;
 
 events.push(function() {
-
+	$('form').submit(function(event){
+		// disable all elements that dont have a value to avoid posting them as it could be sending
+		// more than 5000 variables which is the php default max for less than 100 san's which acme does support
+		// p.s. the jquery .find(['value'='']) would not find newly added empty items) so we use .filter(...)
+		$(this).find(':input').filter(function() { return !this.value }).attr("disabled", "disabled")
+		return true;
+	});
 
 <?php
 	// On gui descriptions when a closetype has been selected..
