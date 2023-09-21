990e9bf87f693cd27ece099f80d2d26bd08e3757
Denny Page <dennypage@me.com>
2023-09-21 12:15:17 -0700

    Move from nut-2.8.0 to nut-devel

--- sysutils/pfSense-pkg-nut/files/usr/local/pkg/nut/nut_email.php
+++ sysutils/pfSense-pkg-nut/files/usr/local/pkg/nut/nut_email.php
@@ -24,8 +24,7 @@
 require_once("notices.inc");
 
 $subject = "UPS Notification from " . gethostname();
-$message = date('r');
-$message .= "\n\n";
+$message = "Date: " . date('r') . "\n\n${subject}" . "\n\n";
 $message .= implode(' ', array_slice($argv, 1));
 
 @notify_all_remote($subject . " - " . $message);
