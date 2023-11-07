cb1cccedfd2504afcd917213cdcdd65acd020fe9
Kristof Provost <kp@FreeBSD.org>
2023-11-07 16:32:45 +0100

    security/sshguard: rebase local patches

--- security/sshguard/files/patch-src_common_attack.h
+++ security/sshguard/files/patch-src_common_attack.h
@@ -1,10 +1,10 @@
---- src/common/attack.h.orig	2020-06-23 23:01:26 UTC
+--- src/common/attack.h.orig	2023-06-27 18:24:15 UTC
 +++ src/common/attack.h
-@@ -45,6 +45,7 @@ enum service {
+@@ -46,6 +46,7 @@ enum service {
      SERVICES_CLF_UNAUTH     = 350,  //< HTTP 401 in common log format
      SERVICES_CLF_PROBES     = 360,  //< probes for common web services
      SERVICES_CLF_LOGIN_URL  = 370,  //< CMS framework logins in common log format
 +    SERVICES_PFSENSE        = 380,  //< pfSense UI login
      SERVICES_OPENVPN        = 400,  //< OpenVPN
+     SERVICES_OPENVPN_PS     = 410,  //< OpenVPN Portshare
      SERVICES_GITEA          = 500,  //< Gitea
- };
