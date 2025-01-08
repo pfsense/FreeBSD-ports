f8c82481287cf5b945ba2bac88f37339257907ba
Kristof Provost <kp@FreeBSD.org>
2025-01-08 09:54:09 +0100

    net/radvd: Regenerate patches for 2.20

--- net/radvd/files/patch-device-common.c
+++ net/radvd/files/patch-device-common.c
@@ -1,5 +1,5 @@
---- device-common.c.orig	2019-07-20 03:58:19 UTC
-+++ device-common.c
+--- device-common.c.orig	2024-12-28 06:27:47.000000000 +0100
++++ device-common.c	2025-01-08 09:51:52.844950000 +0100
 @@ -20,10 +20,24 @@
  
  int check_device(int sock, struct Interface *iface)
@@ -12,7 +12,7 @@
 +	size_t len;
 +
  	memset(&ifr, 0, sizeof(ifr));
- 	strncpy(ifr.ifr_name, iface->props.name, IFNAMSIZ - 1);
+ 	strlcpy(ifr.ifr_name, iface->props.name, sizeof(ifr.ifr_name));
  
 +	if (ioctl(sock, SIOCGIFINDEX, &ifr) < 0) {
 +		flog(LOG_ERR, "ioctl(SIOCGIFINDEX) failed on %s: %s", iface->props.name, strerror(errno));
@@ -25,12 +25,10 @@
  	if (ioctl(sock, SIOCGIFFLAGS, &ifr) < 0) {
  		flog(LOG_ERR, "ioctl(SIOCGIFFLAGS) failed on %s: %s", iface->props.name, strerror(errno));
  		return -1;
-@@ -54,6 +68,43 @@ int check_device(int sock, struct Interface *iface)
- 	} else {
- 		dlog(LOG_ERR, 4, "%s supports multicast or is point-to-point",
+@@ -56,6 +70,43 @@
  		     iface->props.name);
-+	}
-+
+ 	}
+ 
 +	mib[0] = CTL_NET;
 +	mib[1] = PF_LINK;
 +	mib[2] = NETLINK_GENERIC;
@@ -66,6 +64,8 @@
 +		} else {
 +			dlog(LOG_ERR, 4, "%s is active", iface->props.name);
 +		}
- 	}
- 
++	}
++
  	return 0;
+ }
+ 
--- net/radvd/files/patch-includes.h
+++ net/radvd/files/patch-includes.h
@@ -1,11 +1,11 @@
---- includes.h.orig	2019-09-21 21:50:05 UTC
-+++ includes.h
-@@ -81,6 +81,8 @@
- #define IF_NAMESIZE IFNAMSIZ
- #else
+--- includes.h.orig	2025-01-08 09:52:55.082407000 +0100
++++ includes.h	2025-01-08 09:53:14.368065000 +0100
+@@ -80,6 +80,8 @@
+ #endif
+ 
  #include <net/if.h>
 +#include <net/if_media.h>
 +#include <net/if_mib.h>
- #endif
  
  #ifdef HAVE_NET_IF_DL_H
+ #include <net/if_dl.h>
