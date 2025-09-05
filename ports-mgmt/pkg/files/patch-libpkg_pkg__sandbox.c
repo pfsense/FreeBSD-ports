cccb8ec38203c46be6c32f241f62dd4601170890
Luiz Souza <luiz@netgate.com>
2025-09-05 13:55:19 -0300

    ports-mgmt/pkg: only build 50a446cc3efa on systems with the new kernel.
    This workaround is only applicable for the pkg built on systems after
    setgroups() syscall changes.
    
    If built on an old system we want pkg to behave just as before.

--- ports-mgmt/pkg/files/patch-libpkg_pkg__sandbox.c
+++ ports-mgmt/pkg/files/patch-libpkg_pkg__sandbox.c
@@ -1,19 +1,24 @@
 --- libpkg/pkg_sandbox.c.orig	2025-08-05 06:16:40 UTC
 +++ libpkg/pkg_sandbox.c
-@@ -33,6 +33,7 @@
- #include <sys/types.h>
+@@ -34,6 +34,10 @@
  #include <sys/wait.h>
  #include <sys/socket.h>
-+#include <sys/sysctl.h>
  
++#if __FreeBSD_version >= 1500061
++#include <sys/sysctl.h>
++#endif
++
  #ifdef HAVE_CAPSICUM
  #include <sys/capsicum.h>
-@@ -226,13 +227,16 @@ pkg_drop_privileges(void)
+ #endif
+@@ -226,13 +230,20 @@ pkg_drop_privileges(void)
  void
  pkg_drop_privileges(void)
  {
++#if __FreeBSD_version >= 1500061
 +	int osver;
 +	size_t buflen = sizeof(osver);
++#endif
  	struct passwd *nobody;
  
  	if (geteuid() == 0) {
@@ -21,7 +26,9 @@
  		if (nobody == NULL)
  			errx(EXIT_FAILURE, "Unable to drop privileges: no 'nobody' user");
 -		setgroups(1, &nobody->pw_gid);
++#if __FreeBSD_version >= 1500061
 +		if (sysctlbyname("kern.osreldate", &osver, &buflen, NULL, 0) != 0 && osver >= 1500061)
++#endif
 +			setgroups(1, &nobody->pw_gid);
  		/* setgid also sets egid and setuid also sets euid */
  		if (setgid(nobody->pw_gid) == -1)
