f6e20ad7e70492b90813723ba2cf5ea1804996c1
Hiroki Tagato <tagattie@FreeBSD.org>
2026-03-23 16:35:25 +0900

    x11/waveterm: Update to 0.14.3
    Changelog:
    - https://github.com/wavetermdev/waveterm/releases/tag/v0.14.2
    - https://github.com/wavetermdev/waveterm/releases/tag/v0.14.3
    
    Reported by:	GitHub (watch releases)

--- x11/waveterm/files/patch-emain_emain-ipc.ts
+++ x11/waveterm/files/patch-emain_emain-ipc.ts
@@ -1,6 +1,6 @@
---- emain/emain-ipc.ts.orig	2026-02-11 23:55:44 UTC
+--- emain/emain-ipc.ts.orig	2026-03-12 23:29:48 UTC
 +++ emain/emain-ipc.ts
-@@ -337,7 +337,7 @@ export function initIpcHandlers() {
+@@ -354,7 +354,7 @@ export function initIpcHandlers() {
              const color = fac.prepareResult(fac.getColorFromArray4(png.data));
              const ww = getWaveWindowByWebContentsId(event.sender.id);
              ww.setTitleBarOverlay({
