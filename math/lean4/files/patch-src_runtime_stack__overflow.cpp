000e016f64cc0b4fb7a483672c98adc451939c6d
Yuri Victorovich <yuri@FreeBSD.org>
2026-03-28 10:20:55 -0700

    math/lean4: Fix stack overflow test
    All other failing tests fail spuriously due to parallel contention.
    They all pass when run individually.

--- math/lean4/files/patch-src_runtime_stack__overflow.cpp
+++ math/lean4/files/patch-src_runtime_stack__overflow.cpp
@@ -1,4 +1,4 @@
---- src/runtime/stack_overflow.cpp.orig	2025-11-18 02:29:21 UTC
+--- src/runtime/stack_overflow.cpp.orig	2026-03-27 12:45:03 UTC
 +++ src/runtime/stack_overflow.cpp
 @@ -21,6 +21,11 @@ Port of the corresponding Rust code (see links below).
  #include <initializer_list>
@@ -12,3 +12,22 @@
  namespace lean {
  // stack guard of the main thread
  static stack_guard * g_stack_guard;
+@@ -64,8 +69,18 @@ bool is_within_stack_guard(void * addr) {
+ #endif
+     // close enough; the actual guard might be bigger, but it's unlikely a Lean function will have stack frames that big
+     size_t guardsize = static_cast<size_t>(sysconf(_SC_PAGESIZE));
++#if defined(__FreeBSD__)
++    // On FreeBSD the guard page position depends on how the thread was created:
++    // - Main thread with ulimit: guard is below stackaddr (Linux-style, linux_check)
++    // - Spawned threads with explicit stacksize: guard is the first page of the
++    //   region starting at stackaddr (freebsd_check)
++    // Accept either convention.
++    return (stackaddr - guardsize <= addr && addr < stackaddr) ||
++           (stackaddr <= addr && addr < stackaddr + guardsize);
++#else
+     // the stack guard is *below* the stack
+     return stackaddr - guardsize <= addr && addr < stackaddr;
++#endif
+ }
+ 
+ extern "C" LEAN_EXPORT void segv_handler(int signum, siginfo_t * info, void *) {
