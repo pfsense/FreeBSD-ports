938ab55a87e66a3c350c98d3f89d80d703e6e0ec
Nicola Vitale <nivit@FreeBSD.org>
2026-03-28 10:29:13 +0100

    audio/waves: Update to 0.1.43
    - Remove all patches (now included upstream)
    
    ChangeLog:	https://github.com/llehouerou/waves/releases/tag/v0.1.43

--- audio/waves/files/patch-internal_notify_albumart__test.go
+++
@@ -1,8 +0,0 @@
---- internal/notify/albumart_test.go.orig	1979-11-29 23:00:00 UTC
-+++ internal/notify/albumart_test.go
-@@ -1,4 +1,4 @@
--//go:build linux
-+//go:build (linux || freebsd)
- 
- package notify
- 
--- audio/waves/files/patch-internal_notify_dbus__test.go
+++
@@ -1,8 +0,0 @@
---- internal/notify/dbus_test.go.orig	1979-11-29 23:00:00 UTC
-+++ internal/notify/dbus_test.go
-@@ -1,4 +1,4 @@
--//go:build linux
-+//go:build (linux || freebsd)
- 
- package notify
- 
