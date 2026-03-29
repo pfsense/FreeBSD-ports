e1893dcaca06ce6f520e349233ccb4b261c9fe98
Dima Panov <fluffy@FreeBSD.org>
2026-03-30 02:25:50 +0300

    science/votca: add missing #include <cassert>
    Reported by:	bulk -t
    Approved by:	portmgr blanket

---
+++ science/votca/files/patch-xtp_include_votca_xtp_qmstate.h
@@ -0,0 +1,10 @@
+--- xtp/include/votca/xtp/qmstate.h.orig	2025-10-03 18:44:55 UTC
++++ xtp/include/votca/xtp/qmstate.h
+@@ -23,6 +23,7 @@
+ 
+ // Standard includes
+ #include <string>
++#include <cassert>
+ 
+ // VOTCA includes
+ #include <votca/tools/types.h>
