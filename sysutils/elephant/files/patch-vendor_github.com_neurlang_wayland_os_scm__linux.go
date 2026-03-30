ba999350bbb2a980e298b516c020d46397ae3fbf
Hiroki Tagato <tagattie@FreeBSD.org>
2026-03-30 16:28:05 +0900

    sysutils/elephant: Update to 2.20.3
    Changelog: https://github.com/abenz1267/elephant/releases/tag/v2.20.3
    
    Reported by:	GitHub (watch releases)

--- sysutils/elephant/files/patch-vendor_github.com_neurlang_wayland_os_scm__linux.go
+++ sysutils/elephant/files/patch-vendor_github.com_neurlang_wayland_os_scm__linux.go
@@ -1,8 +1,8 @@
---- vendor/github.com/neurlang/wayland/os/scm_linux.go.orig	2025-11-14 04:42:08 UTC
+--- vendor/github.com/neurlang/wayland/os/scm_linux.go.orig	2026-03-30 02:44:23 UTC
 +++ vendor/github.com/neurlang/wayland/os/scm_linux.go
 @@ -1,5 +1,5 @@
--//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
--// +build aix darwin dragonfly freebsd linux netbsd openbsd solaris
+-//go:build aix || dragonfly || freebsd || linux || netbsd || openbsd || solaris
+-// +build aix dragonfly freebsd linux netbsd openbsd solaris
 +//go:build aix || darwin || dragonfly || linux || netbsd || openbsd || solaris
 +// +build aix darwin dragonfly linux netbsd openbsd solaris
  
