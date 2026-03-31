573e07a9d06627c2a6d2bb5eeb0adf9c6d777684
Rene Ladan <rene@FreeBSD.org>
2026-03-31 18:21:58 +0200

    Mk: unregister expired databases/mysql91*

--- Mk/Uses/mysql.mk
+++ Mk/Uses/mysql.mk
@@ -61,7 +61,6 @@ DEFAULT_MYSQL_VER?=	${MYSQL_DEFAULT:S/.//}
 # Mk/bsd.default-versions.mk in sync.
 MYSQL80_LIBVER=		21
 MYSQL84_LIBVER=		24
-MYSQL91_LIBVER=		24
 MYSQL96_LIBVER=		24
 
 .  for v in 106 1011 114 118
