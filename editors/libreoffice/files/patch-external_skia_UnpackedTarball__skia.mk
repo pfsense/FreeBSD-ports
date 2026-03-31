89a804c19f17f9f8eb33e9ce3c523a906beb40b9
Dima Panov <fluffy@FreeBSD.org>
2026-03-31 03:03:31 +0300

    editors/libreoffice: update to 26.2.2 release (+)
    Announce:	https://blog.documentfoundation.org/blog/2026/03/26/libreoffice-26-2-2-and-libreoffice-25-8-6/
    Release notes:	https://wiki.documentfoundation.org/ReleaseNotes/26.2
    Changelog:	https://wiki.documentfoundation.org/Releases/26.2.2/RC1 \
    		https://wiki.documentfoundation.org/Releases/26.2.2/RC2

--- editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
+++ editors/libreoffice/files/patch-external_skia_UnpackedTarball__skia.mk
@@ -1,9 +1,9 @@
 --- external/skia/UnpackedTarball_skia.mk.orig	2025-05-10 18:09:26 UTC
 +++ external/skia/UnpackedTarball_skia.mk
-@@ -44,6 +44,7 @@ skia_patches := \
- 	0004-loong64-Fix-the-remaining-implicit-vector-casts.patch \
+@@ -45,6 +45,7 @@ skia_patches := \
      msvc-unknown-attributes.patch.1 \
  	fix-semaphore-include.patch.1 \
+ 	fix-xputimage-depth.patch.1 \
 +    freebsd-limits.h.patch.0 \
  
  ifneq ($(MSYSTEM),)
